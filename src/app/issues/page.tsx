3c6bb07fd9ad1678dd41c17f6778e35bc34e29b6
sukalov <matvey10@gmail.com>
2023-04-28 03:41:38 +0300

    .experimenting

---
+++ src/app/issues/page.tsx
@@ -0,0 +1,8 @@
+export default function Issues() {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-around p-24">
+        <div className='text-red-500'>issues</div>
+      </main>
+    )
+  }
+  
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+
+ module.exports = {
+  darkMode: 'class',
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
