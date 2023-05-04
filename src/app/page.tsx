38bae9500ceb602f2b8d61d50177fcb90a9e0759
sukalov <matvey10@gmail.com>
2023-05-04 15:31:14 -0600

    padding-decreased

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-centerjustify-around p-36">
+    <main className="flex min-h-screen flex-col items-centerjustify-around p-12">
       <Image className='m-auto' src={logo} alt='mktour' width={200} />
     </main>
   )
