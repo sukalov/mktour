710c7898b8dff0a0d2c6fc40653de21f2f66c53f
Anatoly Yatskov <yatskovanatoly@gmail.com>
2023-07-25 02:03:18 +0300

    Shadsn added and tested

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+'use client'
+
 import logo from './mktour-pascal.gif'
 import Image from 'next/image'
+import { Button } from '@/components/ui/button'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-centerjustify-around p-12">
+    <main className="flex min-h-screen flex-col items-center justify-around p-12">
+      <Button onClick={ () => console.log('clicked') } variant='outline'>Button</Button>
       <Image className='m-auto' src={logo} alt='mktour' width={200} />
     </main>
   )
