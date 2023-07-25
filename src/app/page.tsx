83133775b54f04dfe78ed409baf7774d9e49be82
Anatoly Yatskov <yatskovanatoly@gmail.com>
2023-07-25 21:16:14 +0300

    Button styled

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Image from 'next/image'
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
 import { type ThemeProviderProps } from 'next-themes/dist/types'
 import { Button } from '@/components/ui/button'
+import { SunMoonIcon } from 'lucide-react'
 
 const ThemeProvider = ({ children, ...props }: ThemeProviderProps) => {
 	return <NextThemesProvider {...props}>{children}</NextThemesProvider>
@@ -15,8 +16,8 @@ export default function Home() {
 		<ThemeProvider attribute='class' defaultTheme='dark' enableSystem>
 			<main className='flex min-h-screen flex-col items-center justify-around p-12'>
 				<section style={{display: 'flex', justifyContent: 'flex-end', width: '100%' }}>
-					<Button onClick={() => console.log('clicked')} variant='outline'>
-						Button
+					<Button onClick={() => console.log('clicked')} variant='outline' style={{aspectRatio: 1, padding: '.25rem'}}>
+            <SunMoonIcon/>
 					</Button>
 				</section>
 				<Image className='m-auto' src={logo} alt='mktour' width={200} />
