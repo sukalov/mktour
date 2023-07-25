1f54fef5f5f204a3ea63be16aa95bcfc19fec539
Anatoly Yatskov <yatskovanatoly@gmail.com>
2023-07-25 21:12:35 +0300

    ThemeProvider added + button positioned in corner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,25 @@
 
 import logo from './mktour-pascal.gif'
 import Image from 'next/image'
+import { ThemeProvider as NextThemesProvider } from 'next-themes'
+import { type ThemeProviderProps } from 'next-themes/dist/types'
 import { Button } from '@/components/ui/button'
 
+const ThemeProvider = ({ children, ...props }: ThemeProviderProps) => {
+	return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+}
+
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-around p-12">
-      <Button onClick={ () => console.log('clicked') } variant='outline'>Button</Button>
-      <Image className='m-auto' src={logo} alt='mktour' width={200} />
-    </main>
-  )
+	return (
+		<ThemeProvider attribute='class' defaultTheme='dark' enableSystem>
+			<main className='flex min-h-screen flex-col items-center justify-around p-12'>
+				<section style={{display: 'flex', justifyContent: 'flex-end', width: '100%' }}>
+					<Button onClick={() => console.log('clicked')} variant='outline'>
+						Button
+					</Button>
+				</section>
+				<Image className='m-auto' src={logo} alt='mktour' width={200} />
+			</main>
+		</ThemeProvider>
+	)
 }
