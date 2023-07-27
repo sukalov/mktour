81f9ed3a0cf2d24298406ba43bb8fcd16df89ed0
sukalov <matvey10@gmail.com>
2023-07-27 16:22:15 +0300

    upgrade nextjs + fix hydration error with useffect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,10 @@ import { ModeToggler } from '@/components/mode-toggler'
 export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-around p-12">
-			<section
-				style={{ display: 'flex', justifyContent: 'flex-end', width: '100%' }}
-			>
+			<section style={{ display: 'flex', justifyContent: 'flex-end', width: '100%' }}>
 				<ModeToggler />
 			</section>
-			<Image className="m-auto" src={logo} alt="mktour" width={200} />
+			<Image className="m-auto" src={logo} alt="mktour" width={200} priority />
 		</main>
 	)
 }
