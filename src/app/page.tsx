962913f5c7dbc6603b22db6a70e3cf13fbc81854
Anatoly Yatskov <yatskovanatoly@gmail.com>
2023-07-27 16:28:56 +0300

    mktour logo added as a functional component (LOL)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,20 @@
 'use client'
 
-import logo from './mktour-pascal.gif'
-import Image from 'next/image'
 import { ModeToggler } from '@/components/mode-toggler'
+import mktour from '@/components/ui/mktourLogo'
 
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-around p-12">
+		<main
+			className={`flex min-h-screen flex-col items-center justify-around p-12`}
+		>
 			<section
 				style={{ display: 'flex', justifyContent: 'flex-end', width: '100%' }}
 			>
 				<ModeToggler />
 			</section>
-			<Image className="m-auto" src={logo} alt="mktour" width={200} />
+			{/* <Image className="m-auto" src={logo} alt="mktour" width={200} /> */}
+			{mktour()}
 		</main>
 	)
 }
---
+++ src/components/ui/mktourLogo.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import localFont from 'next/font/local'
+import { useEffect, useState } from 'react'
+
+const turboPascal = localFont({ src: './TurboPascalFont.ttf' })
+
+const mktour = () => {
+	const [isSymbolVisible, setIsSymbolVisible] = useState(true)
+
+	useEffect(() => {
+		const intervalId = setInterval(() => {
+			setIsSymbolVisible((prev) => !prev)
+		}, 500)
+
+		return () => {
+			clearInterval(intervalId)
+		}
+	}, [])
+
+	return (
+		<h1 className={`${turboPascal.className} text-6xl m-auto select-none`}>
+			{isSymbolVisible ? <span>mktour_</span> : <h1>mktour&nbsp;</h1>}
+		</h1>
+	)
+}
+
+export default mktour
