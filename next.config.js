097ac9f72dbcdbd20faf1645616768b91a11fc4f
sukalov <matvey10@gmail.com>
2023-07-27 14:29:45 +0300

    formatted all files as yatskov prefers

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-  strictMode: true
-};
+	experimental: {
+		appDir: true,
+	},
+	strictMode: true,
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
