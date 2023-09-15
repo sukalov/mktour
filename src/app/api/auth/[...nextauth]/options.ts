209ccdac172f4661314dc78b56f408efdd5814ec
sukalov <matvey10@gmail.com>
2023-09-15 22:19:17 +0300

    lichess2 rm

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -4,29 +4,8 @@ import LichessProvider from "@/lib/LichessProvider";
 export const options: NextAuthOptions = {
     providers: [
         LichessProvider({
-            clientId: 'jjhh',
-            clientSecret: 'dfiuwef'
+            clientId: 'mktour.org',
+            clientSecret: process.env.LICHESS_CLIENT_SECRET
         }),
-        {
-            id: "lichess2",
-            name: "Lichess2",
-            type: "oauth",
-            version: "2.0",
-            clientId: 'client-id-test',
-            clientSecret: 'secret',
-            authorization: {
-              url: `https://lichess.org/oauth`,
-              params: { client_id: 'client-id-test', scope: 'email:read', code_challenge_method: 'S256' },
-            },
-            token: `https://lichess.org/api/token`,
-            userinfo: `https://lichess.org/api/account`,
-            checks: ["pkce", "state"],
-            profile(profile) {
-              return {
-                id: profile.id,
-                username: profile.username,
-              };
-            },
-          },
     ],
 }
