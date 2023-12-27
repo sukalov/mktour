1b37db78c5379e87d048a2c042ad5634205965fb
sukalov <matvey10@gmail.com>
2023-12-26 18:25:26 -0700

    style: center loading state inside its component

--- src/components/ui/mktour-logo.tsx
+++ src/components/ui/mktour-logo.tsx
@@ -1,10 +1,12 @@
-import { turboPascal } from "@/app/fonts"
+import { turboPascal } from '@/app/fonts'
 
 export default function Mktour() {
 	return (
-		<h1 className={`${turboPascal.className} text-6xl font-bold m-auto select-none`}>
-			<span>mktour</span>
-			<span className='animate-logo-pulse'>_</span>
-		</h1>
+		<div className='h-svh flex flex-auto items-center justify-center'>
+			<h1 className={`${turboPascal.className} text-6xl font-bold m-auto select-none`}>
+				<span>mktour</span>
+				<span className='animate-logo-pulse'>_</span>
+			</h1>
+		</div>
 	)
 }
