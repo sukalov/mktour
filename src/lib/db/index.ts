d1141857913ab9d63074e11948f122eb902dda92
sukalov <matvey10@gmail.com>
2024-01-07 14:28:00 -0700

    build: move to bun and update dependencies

---
+++ src/lib/db/index.ts
@@ -0,0 +1,6 @@
+import { drizzle } from 'drizzle-orm/libsql';
+import { createClient } from '@libsql/client';
+
+const client = createClient({ url: 'DATABASE_URL', authToken: 'DATABASE_AUTH_TOKEN' });
+
+export const db = drizzle(client);
