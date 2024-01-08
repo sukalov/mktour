fbf8d6cfacb5ef30ccdfde9d8429b86101e2984b
sukalov <matvey10@gmail.com>
2024-01-08 01:51:09 -0700

    feat: desktop home page

--- src/components/home-page.tsx
+++ src/components/home-page.tsx
@@ -8,24 +8,27 @@ export default function HomePage() {
 	const loading = status === 'loading'
 
 	if (!loading) return (
-		<div className='w-full mt-16 h-[calc(100svh-5rem)] p-1 flex flex-col gap-7'>
+		<div className='w-full mt-16 md:mt-2 h-[calc(100svh-5rem)] p-1 flex flex-col gap-7 md:gap-2'>
 			{!session && (
-				<div className='text-[clamp(3rem,8svh,6rem);] leading-none grow font-extrabold w-full m-auto max-w-[min(28rem,95%)] text-balance flex flex-auto items-center'>
+				<div className='text-[clamp(3rem,8svh,6rem);] md:text-[clamp(5rem,10vw,6rem);] leading-none grow font-extrabold w-full m-auto max-w-[min(28rem,95%)] md:max-w-[min(70rem,90%)] text-balance md:text-center flex flex-auto items-center'>
 					<p>chess events has become simple for everyone</p>
 				</div>
 			)}
+			<div className='flex flex-col md:flex-row justify-center items-center gap-4 md:gap-2 md:px-12 max-w- md:mx-auto'>
 			{!session && (
+				<div className='px-1 w-full h-auto m-auto'>
 				<Button
-					className='flex flex-col flex-none gap-2 w-full h-auto min-h-24 font-bold max-w-[28rem] m-auto'
+					className='flex flex-col flex-none gap-2 w-full h-24 min-h-24 font-bold max-w-[28rem] m-auto px-1'
 					variant='outline'
 					onClick={() => signIn('lichess', { redirect: false })}
 				>
 					<div className=' grid-flow-col'></div>
 					<span className=' grid-col-3'>
-						<LichessLogo size='48' />
+						<LichessLogo size='40' />
 					</span>
 					<span className='font-light text-xl grid-col--9'>sign in with lichess</span>
 				</Button>
+				</div>
 			)}
 			<Link href='/new-tournament' className='w-full m-auto px-1'>
 				<Button
@@ -38,6 +41,7 @@ export default function HomePage() {
 					</p>
 				</Button>
 			</Link>
+			</div>
 		</div>
 	)
 	return <></>
