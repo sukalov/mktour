aec6f65a68d5b122f7275999b73f3db306104d21
sukalov <matvey10@gmail.com>
2024-01-08 18:18:47 -0700

    fix: fix broken types agaaain. downgraded react-types back to v18.2.3

--- src/app/tournament/[id]/page.tsx
+++ src/app/tournament/[id]/page.tsx
@@ -1,7 +1,11 @@
-export default async function Tournament ({ params }) {
+import { redis } from "@/lib/db/redis"
+
+export default async function Tournament ({ params }: {params: {id: string}}) {
+    const data = await redis.get(params.id)
     return (
-        <div className="flex flex-auto flex-col items-center justify-center w-full h-svh">
-            {JSON.stringify(params)}
+        <div className="flex flex-auto flex-col gap-6 items-center justify-center w-full h-svh">
+            <p>{JSON.stringify(params)}</p>
+            <div>{JSON.stringify(data, null, 2)}</div>
         </div>
     )
 }
