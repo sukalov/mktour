3be37ff2cb261c2b6c220f793de148fd0d2926a2
sukalov <matvey10@gmail.com>
2024-01-09 08:37:13 -0500

    e73c1ae8c0cab29cae8d35087bfeacd03015055c

--- src/app/new-tournament/type-card.tsx
+++ src/app/new-tournament/type-card.tsx
@@ -1,13 +1,13 @@
 import { Label } from '@/components/ui/label';
 import { RadioGroupItem } from '@/components/ui/radio-group';
+import TypeIcon from '@/components/ui/type-icon';
 import { ReactNode } from 'react';
 
 type TypeCardProps = {
   name: string;
-  children: ReactNode;
 };
 
-export default function TypeCard({ name, children }: TypeCardProps) {
+export default function TypeCard({ name }: TypeCardProps) {
   return (
     <div>
       <RadioGroupItem value={name} id={name} className="peer sr-only" />
@@ -15,7 +15,9 @@ export default function TypeCard({ name, children }: TypeCardProps) {
         htmlFor={name}
         className={`flex h-10 flex-row items-center justify-center gap-2 rounded-md border-2 border-muted bg-popover py-4 text-[0.7rem] hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary sm:text-sm [&:has([data-state=checked])]:border-primary`}
       >
-        <div className={'font-thin'}>{children}</div>
+        <div className={'font-thin'}>
+          <TypeIcon type={name} />
+        </div>
         <span>{name}</span>
       </Label>
     </div>
