4985e00d77d9c96c8613b218d618fd16f5f205c7
sukalov <matvey10@gmail.com>
2024-01-08 18:32:54 -0700

    fix: update env files names

--- src/lib/db/redis.ts
+++ src/lib/db/redis.ts
@@ -1,6 +1,6 @@
 import { Redis } from '@upstash/redis';
 
 export const redis = new Redis({
-  url: String(process.env.REDIS_URL),
-  token: String(process.env.REDIS_TOKEN),
+  url: String(process.env.UPSTASH_REDIS_REST_URL),
+  token: String(process.env.UPSTASH_REDIS_REST_TOKEN),
 });
