3f82188c06add2626a6624e825e6a927986e4c1b
sukalov <matvey10@gmail.com>
2024-01-13 14:02:54 +0300

    fix: tournament page coonnected back to redis

--- src/app/tournament/[id]/page.tsx
+++ src/app/tournament/[id]/page.tsx
@@ -10,30 +10,14 @@ export default async function Tournament({
 }) {
   let data: RedisTournamentInfo;
   try {
-    // data = await redis.get(params.id) as RedisTournamentInfo;
-    data = {
-      tournament: 'test tournament',
-      date: new Date('2024-01-09T08:58:56.329Z'),
-      format: 'single elimination',
-      type: 'solo',
-      timestamp: '2024-01-09T08:59:11.487Z',
-      user: 'sukalov',
-    };
+    data = await redis.get(params.id) as RedisTournamentInfo;
   } catch (e) {
     console.log(e);
-    data = {
-      tournament: 'test tournament',
-      date: new Date('2024-01-09T08:58:56.329Z'),
-      format: 'single elimination',
-      type: 'solo',
-      timestamp: '2024-01-09T08:59:11.487Z',
-      user: 'sukalov',
-    };
   }
   return (
     <div className="p-2 pt-16">
       {/* @ts-expect-error Server Component */}
-      <TournamentInfo data={data} />
+      {data && <TournamentInfo data={data} />}
       {/* <p>{JSON.stringify(params)}</p> */}
       {/* {data && <div>{JSON.stringify(data, null, 2)}</div>} */}
     </div>
