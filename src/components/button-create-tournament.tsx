5380789bd3fa35f5495ecfac691a6365b0e82e75
Anatoly Yatskov <yatskovanatoly@gmail.com>
2024-01-28 10:22:09 +0300

    Minor create-tournament styling

---
+++ src/components/button-create-tournament.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+
+const CreateTournamentButton = () => {
+  return (
+    <Link href="/new-tournament" className="m-auto w-full px-1">
+      <Button
+        className="m-auto flex h-28 min-h-28 w-full max-w-[28rem] flex-col gap-2 font-bold"
+        variant="default"
+      >
+        <h1 className=" text-2xl font-light min-[320px]:text-3xl">
+          make tournament
+        </h1>
+      </Button>
+    </Link>
+  );
+};
+
+export default CreateTournamentButton;
