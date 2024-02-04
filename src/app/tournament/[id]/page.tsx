8c5a1774a9ea83f961560ad24bcb1b877f802bbd
sukalov <matvey10@gmail.com>
2024-02-05 01:05:43 +0300

    feat: working on tournament to websocket connection

--- src/app/tournament/[id]/page.tsx
+++ src/app/tournament/[id]/page.tsx
@@ -1,3 +1,4 @@
+import { TournamentDashboard } from '@/app/tournament/[id]/tournament-dashboard';
 import { getUser } from '@/lib/auth/utils';
 import { db } from '@/lib/db';
 import {
@@ -8,6 +9,7 @@ import {
   tournaments,
 } from '@/lib/db/schema/tournaments';
 import { and, eq } from 'drizzle-orm';
+import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
 
 export const revalidate = 0;
@@ -37,21 +39,21 @@ export default async function TournamentPage({ params }: TournamentPageProps) {
 
     if (status === undefined) redirect(`/tournament/${params.id}/view`);
 
-  // const req = await fetch('http://localhost:4000', {
-  //   next: { revalidate: 0 },
-  //   headers: {
-  //     Authorization: `Bearer ${cookies().get('token')?.value}`,
-  //   },
-  // });
+  const req = await fetch('http://localhost:8080/', {
+    next: { revalidate: 0 },
+    headers: {
+      Authorization: `Bearer ${cookies().get('token')?.value}`,
+    },
+  });
   // const res = await req.json();
-  // console.log(res);
+  // console.log(req);
   // const room = res.status === 404 ? null : res;
-
-  // fetch user session for server rendering
 
   return (
     <div className="flex w-full flex-col items-start justify-between gap-4">
       <pre>{JSON.stringify({ user, tournament, club, status }, null, 2)}</pre>
+      <pre>{JSON.stringify({ req }, null, 2)}</pre>
+      <TournamentDashboard tournamentId={tournament.id}/>
     </div>
   );
 }
