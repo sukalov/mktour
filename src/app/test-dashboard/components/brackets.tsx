c24326ceba9123e483fbab4c652b61ab2be13255
Anatoly Yatskov <yatskovanatoly@gmail.com>
2024-02-07 01:07:52 +0300

    Got rid of Joystick (too complicated)

--- src/app/test-dashboard/components/brackets.tsx
+++ src/app/test-dashboard/components/brackets.tsx
@@ -1,8 +1,6 @@
 import { players } from '@/app/test-dashboard/components/mocks';
 import { Card } from '@/components/ui/card';
-import { SetStateAction, useEffect, useState } from 'react';
-import { Joystick } from 'react-joystick-component';
-import OutsideClickHandler from 'react-outside-click-handler';
+import { SetStateAction, useState } from 'react';
 
 export default function Brackets({
   handleResult,
@@ -11,7 +9,6 @@ export default function Brackets({
   const [result, setResult] = useState<number | null>(null);
   const [pos, setPos] = useState<any>({});
   const [pressed, setPressed] = useState(false);
-  const color = 'hsl(var(--muted-foreground))';
 
   // const bind = useLongPress(
   //   useCallback(() => {
@@ -23,33 +20,10 @@ export default function Brackets({
   //   },
   // );
   const onEdit = !pressed ? '' : 'border-green-900';
-  const handleClick = (type: string) => {
-    if (type === 'start') {
-      setPressed(true);
-      setActive(false);
-    } else {
-      setPressed(false);
-      setActive(true);
-    }
-  };
-
-  useEffect(() => {
-    if (pos.direction === 'LEFT' && pos.distance === 100) {
-      setResult(-1);
-    }
-    if (pos.direction === 'FORWARD' && pos.distance === 100) {
-      setResult(0);
-    }
-    if (pos.direction === 'BACKWARD' && pos.distance === 100) {
-      setResult(1);
-    }
-    if (pos.direction === 'RIGHT' && pos.distance === 100) {
-      setResult(null);
-    }
-  }, [pos.direction, pos.distance]);
 
   return (
     <Card
+      id="resultCard"
       className={`flex flex-row items-center border ${onEdit} justify-between p-4`}
     >
       <Pair
@@ -59,45 +33,23 @@ export default function Brackets({
         player1={players[0]}
         player2={players[1]}
       />
-      <OutsideClickHandler onOutsideClick={() => setActive(true)}>
-        {/* <div {...bind()}> */}
-        <Joystick
-          // disabled={!pressed}
-          start={() => handleClick('start')}
-          stop={() => handleClick('')}
-          size={50}
-          baseColor=""
-          stickColor={color}
-          throttle={100}
-          move={(pos) => setPos(pos)}
-        />
-        {/* </div> */}
-      </OutsideClickHandler>
     </Card>
   );
 }
 
-const Pair: React.FC<PairProps> = ({
-  result,
-  setResult,
-  handleResult,
-  player1,
-  player2,
-}) => {
-  const [submit, setSubmit] = useState(false);
-
+const Pair: React.FC<PairProps> = ({ result, player1, player2 }) => {
   return (
     <div className="flex flex-row justify-between gap-4">
       <div className="flex flex-col gap-6">
         <div>{player1.name}</div>
         <div>{player2.name}</div>
       </div>
-      <Result setSubmit={setSubmit} result={result} />
+      <Result result={result} />
     </div>
   );
 };
 
-const Result = ({ handleClick, setSubmit, result }: any) => {
+const Result = ({ result }: any) => {
   const resultP1 = result === 0 ? '1' : result === -1 ? '½' : '0';
   const resultP2 = result === 0 ? '0' : result === -1 ? '½' : '1';
 
