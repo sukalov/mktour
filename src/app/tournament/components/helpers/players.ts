91a092cb43771a312ffc3533aa0a8717952bf0db
Anatoly Yatskov <yatskovanatoly@gmail.com>
2024-03-11 01:47:47 +0300

    UseMediaQuery added

--- src/app/tournament/components/helpers/players.ts
+++ src/app/tournament/components/helpers/players.ts
@@ -3,7 +3,7 @@ import { Player } from '@/app/tournament/[id]/tournament-context';
 const length = 16
 
 export const playersArray: Player[] = Array.from({ length }, (_, i) => ({
-  name: `player${i + 1}`,
+  name: `playerWithLongName${i + 1}`,
   win: 0,
   loose: 0,
   draw: 0,
--- src/app/tournament/components/rounds-container.tsx
+++ src/app/tournament/components/rounds-container.tsx
@@ -4,13 +4,17 @@ import {
 } from '@/app/tournament/[id]/tournament-context';
 import RoundItem from '@/app/tournament/components/round-item';
 import { FC, useContext } from 'react';
+import { useMediaQuery } from 'react-responsive';
 
 const RoundsContainer: FC = () => {
   const { games, currentRound } = useContext(TournamentContext);
+  const isFullWidth = useMediaQuery({minWidth: '1224px'})
+  console.log(isFullWidth)
+  const preparedGames = games.slice(games.length - 6)
   return (
-    <div className="flex overflow-scroll">
-      {games.map((round: GameType[], roundIndex: number) => (
-        <div className="flex flex-col justify-center" key={roundIndex}>
+    <div className="flex justify-between px-4">
+      {preparedGames.map((round: GameType[], roundIndex: number) => (
+        <div className="flex flex-col justify-center text-center" key={roundIndex}>
           Round {roundIndex} {roundIndex === currentRound && '(current)'}
           <RoundItem key={roundIndex} round={round} />
         </div>
