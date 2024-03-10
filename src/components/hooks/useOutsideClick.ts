8268f3afd65879f3ae5500920f9a02c852c6452a
Anatoly Yatskov <yatskovanatoly@gmail.com>
2024-03-11 01:49:40 +0300

    Custom useOutsideClick added

---
+++ src/components/hooks/useOutsideClick.ts
@@ -0,0 +1,21 @@
+import { RefObject, useEffect } from 'react'
+
+const useOutsideClick = (
+	handler: () => void,
+	ref: RefObject<any>
+) => {
+	useEffect(() => {
+		const handleClickOutside = (event: MouseEvent | TouchEvent) => {
+			if (ref.current && !ref.current.contains(event.target))
+				handler()
+		}
+		document.addEventListener('mousedown', handleClickOutside, {capture: true})
+    window.addEventListener('touchend', handleClickOutside, {capture: true})
+		return () => {
+      document.removeEventListener('mousedown', handleClickOutside, {capture: true})
+      window.removeEventListener('touchend', handleClickOutside, {capture: true})
+    }
+	}, [handler, ref])
+}
+
+export default useOutsideClick
