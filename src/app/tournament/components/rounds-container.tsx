09acb29e2187abf4f1749b0127cd59f2d0ecb7f4
Anatoly Yatskov <yatskovanatoly@gmail.com>
2024-03-17 04:46:18 +0300

    Removed react-responsive

--- src/app/tournament/components/rounds-container.tsx
+++ src/app/tournament/components/rounds-container.tsx
@@ -4,12 +4,9 @@ import {
 } from '@/app/tournament/[id]/tournament-context';
 import RoundItem from '@/app/tournament/components/round-item';
 import { FC, useContext } from 'react';
-import { useMediaQuery } from 'react-responsive';
 
 const RoundsContainer: FC = () => {
   const { games, currentRound } = useContext(TournamentContext);
-  const isFullWidth = useMediaQuery({minWidth: '1224px'})
-  console.log(isFullWidth)
   const preparedGames = games.slice(games.length - 6)
   return (
     <div className="flex justify-between px-4">
