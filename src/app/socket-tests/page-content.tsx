fdd4a32fd5deab3e02cbab640cdd2d472be7f7da
sukalov <matvey10@gmail.com>
2024-05-07 17:10:44 -0600

    build: wrap clientpage with server rendered cookie provider and attach session to the ws connection request

---
+++ src/app/socket-tests/page-content.tsx
@@ -0,0 +1,62 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { SOCKET_URL } from '@/config/urls';
+import { useRef, useState } from 'react';
+import useSWRSubscription from 'swr/subscription';
+
+export const dynamic = 'force-dynamic';
+
+interface SocketTestsProps {
+    session: string
+}
+
+export default function SocketTests({session}: SocketTestsProps) {
+  const [messages, setMessages] = useState<string[]>([])
+  const ws = useRef<WebSocket>();
+
+  const { data, error } = useSWRSubscription(
+    `${SOCKET_URL}/randomTournamentId?auth_session=${session}`,
+    (key, { next }) => {
+      ws.current = new WebSocket(key);
+      ws.current.onopen = () => {
+        pingpong();
+        console.log('WebSocket Connection');
+      };
+      ws.current.onmessage = (event) => {
+        console.log({ data, error });
+        next(null, event.data);
+        console.log(event.data);
+        setMessages(prev => prev.concat(event.data))
+      };
+      ws.current.onerror = (error) => next(error, null);
+      return () => ws.current?.close();
+    },
+  );
+
+  function pingpong() {
+    if (!ws.current) return;
+    if (ws.current.readyState !== 1) return;
+    ws.current.send('ping');
+    setTimeout(pingpong, 2000);
+  }
+
+  const sendMessage = (formData: FormData) => {
+    if (!ws.current) {console.log('NOTSENT: ' + formData.get('message')); return }
+    ws.current.send(formData.get('message') ?? '');
+    setMessages(prev => [...prev, `me: ${formData.get('message') ?? ''}`]);
+  };
+
+  return (
+    <div>
+      <form action={sendMessage} className='p-4'>
+        <Input name="message" className='max-w-md w-full' placeholder='test message'/>
+        <Button type="submit" className=' mt-4 w-full max-w-md'>send</Button>
+      </form>
+      {messages.map((msg: string, i) => (
+        <p key={i}>{msg}</p>
+      ))}
+    </div>
+  );
+}
--- src/app/socket-tests/page.tsx
+++ src/app/socket-tests/page.tsx
@@ -1,58 +1,11 @@
-'use client';
+import SocketTests from '@/app/socket-tests/page-content';
+import { cookies } from 'next/headers';
 
-import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
-import { SOCKET_URL } from '@/config/urls';
-import { useRef, useState } from 'react';
-import useSWRSubscription from 'swr/subscription';
-
-export const dynamic = 'force-dynamic';
-
-export default function SocketTests() {
-  const [messages, setMessages] = useState<string[]>([])
-  const ws = useRef<WebSocket>();
-
-  const { data, error } = useSWRSubscription(
-    `${SOCKET_URL}/randomTournamentId`,
-    (key, { next }) => {
-      ws.current = new WebSocket(key);
-      ws.current.onopen = () => {
-        pingpong();
-        console.log('WebSocket Connection');
-      };
-      ws.current.onmessage = (event) => {
-        console.log({ data, error });
-        next(null, event.data);
-        console.log(event.data);
-        setMessages(prev => prev.concat(event.data))
-      };
-      ws.current.onerror = (error) => next(error, null);
-      return () => ws.current?.close();
-    },
-  );
-
-  function pingpong() {
-    if (!ws.current) return;
-    if (ws.current.readyState !== 1) return;
-    ws.current.send('ping');
-    setTimeout(pingpong, 2000);
-  }
-
-  const sendMessage = (formData: FormData) => {
-    if (!ws.current) {console.log('NOTSENT: ' + formData.get('message')); return }
-    ws.current.send(formData.get('message') ?? '');
-    setMessages(prev => [...prev, `me: ${formData.get('message') ?? ''}`]);
-  };
-
+export default function SocketTestsPage() {
+  const session = cookies().get('auth_session')?.value ?? ''
   return (
     <div>
-      <form action={sendMessage} className='p-4'>
-        <Input name="message" className='max-w-md w-full' placeholder='test message'/>
-        <Button type="submit" className=' mt-4 w-full max-w-md'>send</Button>
-      </form>
-      {messages.map((msg: string, i) => (
-        <p key={i}>{msg}</p>
-      ))}
+      <SocketTests session={session}/>
     </div>
   );
 }
