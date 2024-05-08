6ba8b3486e43d7128a4518e09343b0aed7f6ded7
sukalov <matvey10@gmail.com>
2024-05-08 17:41:36 -0600

    fix: redirect to notFound if !tournament

--- src/app/tournament/[id]/view/page.tsx
+++ src/app/tournament/[id]/view/page.tsx
@@ -1,9 +1,11 @@
 import Dashboard from '@/app/tournament/[id]/dashboard';
 import { TournamentPageProps } from '@/app/tournament/[id]/page';
 import useTournamentToClubQuery from '@/lib/db/hooks/useTournamentToClubQuery';
+import { notFound } from 'next/navigation';
 
 export default async function TournamentView({ params }: TournamentPageProps) {
   const { tournament } = await useTournamentToClubQuery({ params });
+  if (!tournament) notFound()
 
   return (
     <div className="w-full">
