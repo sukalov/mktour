cf9b7793d96fd5b3095638ca2f735ba2bca91ea7
sukalov <matvey10@gmail.com>
2024-05-10 19:08:09 -0600

    feat: socket-tests rewritten with using react-use-websocket hook

--- src/app/socket-tests/page-content.tsx
+++ src/app/socket-tests/page-content.tsx
@@ -3,8 +3,8 @@
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { SOCKET_URL } from '@/lib/config/urls';
-import { useRef, useState } from 'react';
-import useSWRSubscription from 'swr/subscription';
+import { useState } from 'react';
+import useWebSocket from 'react-use-websocket';
 
 export const dynamic = 'force-dynamic';
 
@@ -13,52 +13,69 @@ interface SocketTestsProps {
 }
 
 export default function SocketTests({ session }: SocketTestsProps) {
+  const {
+    sendMessage,
+    // sendJsonMessage,
+    // lastMessage,
+    // lastJsonMessage,
+    // readyState,
+    // getWebSocket,
+  } = useWebSocket(`${SOCKET_URL}/test-usews-hook`, {
+    queryParams: {
+      auth_session: session,
+    },
+    onOpen: () => console.log('opened'),
+    shouldReconnect: () => true,
+    heartbeat: {
+      interval: 5000,
+    },
+    onMessage: (event) => {
+      setMessages((prev) => prev.concat(event.data));
+    },
+  });
+
   const [messages, setMessages] = useState<string[]>([]);
-  const ws = useRef<WebSocket>();
-  const inputRef = useRef<HTMLInputElement | null>(null);
+  // const ws = useRef<WebSocket>();
+  // const inputRef = useRef<HTMLInputElement | null>(null);
 
   //   const { data, error } = useSWRSubscription(
-  useSWRSubscription(
-    `${SOCKET_URL}/randomTournamentId?auth_session=${session}`,
-    (key, { next }) => {
-      ws.current = new WebSocket(key);
-      ws.current.onopen = () => {
-        pingpong();
-      };
-      ws.current.onmessage = (event) => {
-        next(null, event.data);
-        setMessages((prev) => prev.concat(event.data));
-      };
-      ws.current.onerror = (error) => next(error, null);
-      return () => ws.current?.close();
-    },
-  );
+  // useSWRSubscription(
+  //   `${SOCKET_URL}/randomTournamentId?auth_session=${session}`,
+  //   (key, { next }) => {
+  //     ws.current = new WebSocket(key);
+  //     ws.current.onopen = () => {
+  //       pingpong();
+  //     };
+  //     ws.current.onmessage = (event) => {
+  //       next(null, event.data);
+  //       setMessages((prev) => prev.concat(event.data));
+  //     };
+  //     ws.current.onerror = (error) => next(error, null);
+  //     return () => ws.current?.close();
+  //   },
+  // );
 
-  function pingpong() {
-    if (!ws.current) return;
-    if (ws.current.readyState !== 1) return;
-    ws.current.send('ping');
-    setTimeout(pingpong, 2000);
-  }
+  // function pingpong() {
+  //   if (!ws.current) return;
+  //   if (ws.current.readyState !== 1) return;
+  //   ws.current.send('ping');
+  //   setTimeout(pingpong, 2000);
+  // }
 
-  const sendMessage = (formData: FormData) => {
-    if (!ws.current) {
-      console.log("we're offline");
-      return;
-    }
-    inputRef!.current!.value = '';
-    ws.current.send(formData.get('message') ?? '');
+  const sendWSMessage = (formData: FormData) => {
+    // inputRef!.current!.value = '';
+    sendMessage(formData.get('message') ?? '');
     setMessages((prev) => [...prev, `me: ${formData.get('message')}`]);
   };
 
   return (
     <div>
-      <form action={sendMessage} className="p-4">
+      <form action={sendWSMessage} className="p-4">
         <Input
           name="message"
           className="w-full max-w-md"
           placeholder="test message"
-          ref={inputRef}
+          // ref={inputRef}
           minLength={1}
           required
         />
