188b91f88d20b94750fcfad03d97c50b73d532d0
sukalov <matvey10@gmail.com>
2024-05-15 23:18:04 -0600

    temporary change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 import Authorized from '@/app/(routes)/authorized';
-import Unauthorized from '@/app/(routes)/unauthorized';
 import { validateRequest } from '@/lib/auth/lucia';
 import '@/styles/cursor.css';
 
 export default async function HomePage() {
   const { user } = await validateRequest();
 
-  if (!user) return <Unauthorized />;
+  // if (!user) return <Unauthorized />;
   return <Authorized user={user} />;
 }
