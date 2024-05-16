cb57c9437691738abbb326ce7859077d28157747
sukalov <matvey10@gmail.com>
2024-05-15 23:12:46 -0600

    fix: same fix extended

--- src/app/tournament/[id]/view/page.tsx
+++ src/app/tournament/[id]/view/page.tsx
@@ -8,11 +8,15 @@ import { notFound } from 'next/navigation';
 
 export default async function TournamentView({ params }: TournamentPageProps) {
   const user = await getUser();
-  const state = await getTournamentState(params.id);
+  let state;
+
+  try {
+    state = await getTournamentState(params.id);
+  } catch {
+    notFound();
+  }
   const session = cookies().get('auth_session')?.value ?? '';
   let status = await useStatusInTournament(user, params.id);
-
-  if (!state) notFound();
 
   return (
     <div className="w-full">
