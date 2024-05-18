15bc6ea636549c2472d3b71ffd5ec15f3c22604d
Anatoly <yatskovanatoly@gmail.com>
2024-05-19 01:56:58 +0300

    Feat: Skeleton for Dashboard Main

--- src/components/dashboard/tabs/main/index.tsx
+++ src/components/dashboard/tabs/main/index.tsx
@@ -1,20 +1,23 @@
 import { Card } from '@/components/ui/card';
+import { Skeleton } from '@/components/ui/skeleton';
 import { useTournamentStore } from '@/lib/hooks/use-tournament-store';
 import { CalendarDays, Dices, UserRound } from 'lucide-react';
 import { FC, ReactNode } from 'react';
 
 const Main = () => {
-  const { ...tournament } = useTournamentStore();
+  const { title, type, format, date, isLoading } = useTournamentStore();
+
+  if (isLoading) return <LoadingElement />
 
   return (
     <div className="flex flex-col gap-4 p-4">
       <div className="truncate whitespace-break-spaces text-4xl font-bold">
-        {tournament?.title}
+        {title}
       </div>
       <Card className="flex w-full flex-col items-center gap-8 p-4">
-        <InfoItem icon={<UserRound />} value={tournament?.type} />
-        <InfoItem icon={<Dices />} value={tournament?.format} />
-        <InfoItem icon={<CalendarDays />} value={tournament?.date} />
+        <InfoItem icon={<UserRound />} value={type} />
+        <InfoItem icon={<Dices />} value={format} />
+        <InfoItem icon={<CalendarDays />} value={date} />
       </Card>
     </div>
   );
@@ -29,5 +32,18 @@ const InfoItem: FC<{
     {value}
   </div>
 );
+
+const LoadingElement = () => {
+  return (
+    <div className="flex flex-col gap-4 p-4">
+      <Skeleton className="h-10 w-full" />
+      <Card className="flex w-full flex-col items-center gap-8 p-4">
+        <Skeleton className="h-6 w-[150px]" />
+        <Skeleton className="h-6 w-[200px]" />
+        <Skeleton className="h-6 w-[250px]" />
+      </Card>
+    </div>
+  );
+};
 
 export default Main;
