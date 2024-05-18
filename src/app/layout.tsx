29b16bf0f26420f4c45f36c5ca7680a30d54af23
Anatoly <yatskovanatoly@gmail.com>
2024-05-19 02:57:50 +0300

    Feat!: disable ALL pinch-zoom action

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ function RootLayout({ children }: PropsWithChildren) {
     <html lang="en" suppressHydrationWarning>
       <meta
         name="viewport"
-        content="width=device-width, initial-scale=1.0, maximum-scale=1.0"
+        content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
       />
       <AxiomWebVitals />
       <body>
