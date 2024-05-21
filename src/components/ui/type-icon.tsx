9b86f2bb17db3811d37d5511d00ec61cc560570b
sukalov <matvey10@gmail.com>
2024-05-21 03:43:00 -0600

    fix: fix icon-colors moving svgs to react

--- src/components/ui/type-icon.tsx
+++ src/components/ui/type-icon.tsx
@@ -1,3 +1,4 @@
+import { Group, Groups, Person } from "@/components/icons";
 import Icon from "@/components/ui/icon";
 
 interface TypeIconProps {
@@ -5,9 +6,9 @@ interface TypeIconProps {
 }
 
 export default function TypeIcon({ type }: TypeIconProps) {
-  if (type === 'solo') return <Icon name='person' className="inline -translate-y-[1px]" width={17} height={17} />;
+  if (type === 'solo') return <Person className="inline -translate-y-[1px]" width={17} height={17}/>
   if (type === 'doubles')
-    return <Icon name='group' className="inline -translate-y-[1px]" width={17} height={17}  />;
-  if (type === 'team') return <Icon name='groups' className="inline pr-0.5 -translate-y-[2px]" width={26} height={26}  />;
+    return <Group className="inline -translate-y-[1px]" width={17} height={17}  />;
+  if (type === 'team') return <Groups className="inline pr-0.5 -translate-y-[2px]" width={26} height={26}  />;
   else return <></>;
 }
