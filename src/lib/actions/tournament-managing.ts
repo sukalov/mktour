e92f93a4d1069064c2db14d41249a9ea95a0a9c2
sukalov <matvey10@gmail.com>
2024-05-23 20:54:41 -0600

    fix: tournament is created with selected club

--- src/lib/actions/tournament-managing.ts
+++ src/lib/actions/tournament-managing.ts
@@ -4,25 +4,15 @@ import { validateRequest } from '@/lib/auth/lucia';
 import { db } from '@/lib/db';
 import {
   DatabaseTournament,
-  clubs_to_users,
-  tournaments,
+  tournaments
 } from '@/lib/db/schema/tournaments';
 import { newid } from '@/lib/utils';
 import { NewTournamentFormType } from '@/lib/zod/new-tournament-form';
-import { and, eq } from 'drizzle-orm';
 import { redirect } from 'next/navigation';
 
 export const createTournament = async (values: NewTournamentFormType) => {
   const { user } = await validateRequest();
   const newTournamentID = newid();
-  const club_id =
-    (
-      await db
-        .select()
-        .from(clubs_to_users)
-        .where(and(eq(clubs_to_users.user_id, user!.id)))
-    ).at(0)?.club_id ?? null;
-
   const newTournament: DatabaseTournament = {
     ...values,
     date: new Date(values.date).toISOString().slice(0, 10),
@@ -30,7 +20,7 @@ export const createTournament = async (values: NewTournamentFormType) => {
     created_at: new Date(),
     closed_at: null,
     started_at: null,
-    club_id: String(club_id),
+    club_id: values.club_id,
     rounds_number: null,
     ongoing_round: 1,
     rated: values.rated,
