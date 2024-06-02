1c24702fa9267ae4c2f28669fb583c032bc2d6a2
sukalov <matvey10@gmail.com>
2024-06-02 15:18:41 -0600

    feat: add local database for offline dev mode

--- src/lib/config/urls.ts
+++ src/lib/config/urls.ts
@@ -7,3 +7,11 @@ export const SOCKET_URL =
   process.env.NODE_ENV === 'production'
     ? process.env.NEXT_PUBLIC_SOCKET_URL
     : 'ws://localhost:7070';
+
+export const DATABASE_URL =
+  process.env.OFFLINE === 'true'
+    ? 'http://localhost:8080'
+    : process.env.DATABASE_URL;
+
+export const DATABASE_AUTH_TOKEN =
+  process.env.OFFLINE === 'true' ? '' : process.env.DATABASE_AUTH_TOKEN;
--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,11 +1,12 @@
+import { DATABASE_AUTH_TOKEN, DATABASE_URL } from '@/lib/config/urls';
 import * as schema1 from '@/lib/db/schema/auth';
 import * as schema2 from '@/lib/db/schema/tournaments';
 import { createClient } from '@libsql/client';
 import { drizzle } from 'drizzle-orm/libsql';
 
 const config = {
-  url: process.env.DATABASE_URL!,
-  authToken: process.env.DATABASE_AUTH_TOKEN,
+  url: DATABASE_URL,
+  authToken: DATABASE_AUTH_TOKEN,
 };
 
 export const sqlite = createClient(config);
