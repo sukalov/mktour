24e951ba2cf573a0109c34a4709912a374610749
Anatoly <yatskovanatoly@gmail.com>
2024-06-07 15:32:27 +0300

    Fix: typesafe validate and redirect to sign-in when !user

--- src/app/club/dashboard/page.tsx
+++ src/app/club/dashboard/page.tsx
@@ -2,10 +2,11 @@ import Main from '@/app/club/dashboard/main';
 import { validateRequest } from '@/lib/auth/lucia';
 import useUserToClubsQuery from '@/lib/db/hooks/use-user-to-clubs-query';
 import { DatabaseClub } from '@/lib/db/schema/tournaments';
-import { User } from 'lucia';
+import { redirect } from 'next/navigation';
 
 export default async function ClubPage() {
-  const { user } = await validateRequest() as { user: User }
+  const { user } = await validateRequest();
+  if (!user) redirect('/sign-in');
   const userClubs = await useUserToClubsQuery({ user });
   const clubs = userClubs.map((club) => club.club) as DatabaseClub[];
   return <Main clubs={clubs} selected={user.selected_club} user={user} />;
