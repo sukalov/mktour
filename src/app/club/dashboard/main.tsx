88eaf1ee17707ccb97638b1421de030a08318db6
Anatoly <yatskovanatoly@gmail.com>
2024-06-17 20:02:55 +0300

    Feat: club select

--- src/app/club/dashboard/main.tsx
+++ src/app/club/dashboard/main.tsx
@@ -1,62 +1,101 @@
 'use client';
 
-import ClubSelectDrawer from '@/app/club/dashboard/club-select-drawer';
 import { InfoItem } from '@/components/dashboard/tabs/main';
-import { Button } from '@/components/ui/button';
 import { Card } from '@/components/ui/card';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+import handleClubSelect from '@/lib/actions/club-select';
 import { DatabaseClub } from '@/lib/db/schema/tournaments';
 import { User } from 'lucia';
-import { CalendarDays, Info, Settings } from 'lucide-react';
+import { CalendarDays, Info, Loader2 } from 'lucide-react';
 import { useRouter } from 'next/navigation';
-import { FC, ReactNode } from 'react';
+import { FC, useEffect, useState } from 'react';
 
 const Main: FC<{ clubs: DatabaseClub[]; selected: string; user: User }> = ({
   clubs,
   selected,
   user,
 }) => {
+  const [loading, setLoading] = useState(false);
   const selectedClub = clubs.find((club) => club.id === selected) || clubs[0];
-  const selectNew = (
-    <ClubSelectDrawer clubs={clubs} selected={selectedClub} user={user} />
-  );
-  return <ClubInfo club={selectedClub} selectNew={selectNew} />;
-};
+  const router = useRouter();
 
-const ClubInfo = ({
-  club,
-  selectNew,
-}: {
-  club: DatabaseClub;
-  selectNew: ReactNode;
-}) => {
-  const { created_at, name, description } = club;
-  const navigate = useRouter();
-  const createdAt = created_at?.toLocaleDateString(['en-GB'], {
-    dateStyle: 'medium',
-  });
+  useEffect(() => {
+    setLoading(false);
+  }, [clubs]);
+
+  const handleChange = (clubId: string) => {
+    if (clubId !== selectedClub.id) {
+      setLoading(true);
+      handleClubSelect(clubId, user.id);
+      router.refresh();
+    }
+  };
 
   return (
     <div className="flex w-full flex-col gap-2">
-      <div className="truncate whitespace-break-spaces text-balance text-4xl font-bold">
-        {name}
-        <span className="whitespace-nowrap">
-          {selectNew}
-          <Button
-            size={'icon'}
-            variant={'ghost'}
-            style={{ backgroundColor: 'transparent' }}
-            onClick={() => navigate.push('dashboard/settings')}
-          >
-            <Settings />
-          </Button>
-        </span>
-      </div>
-      <Card className="items-left flex w-full flex-col gap-8 p-4">
-        {description && <InfoItem icon={<Info />} value={description} />}
-        <InfoItem icon={<CalendarDays />} value={createdAt} />
-      </Card>
+      <ClubSelect
+        clubs={clubs}
+        selectedClub={selectedClub}
+        handleChange={handleChange}
+      />
+      <ClubInfo selectedClub={selectedClub} loading={loading} />
     </div>
   );
+};
+
+const ClubInfo: FC<{ selectedClub: DatabaseClub; loading: boolean }> = ({
+  selectedClub,
+  loading,
+}) => {
+  const createdAt = selectedClub.created_at?.toLocaleDateString(['en-GB'], {
+    dateStyle: 'medium',
+  });
+
+  if (loading)
+    return <Loader2 className="mt-[25%] h-16 w-16 animate-spin self-center" />;
+  return (
+    <Card className="items-left flex w-full flex-col gap-8 p-4">
+      {selectedClub.description && (
+        <InfoItem icon={<Info />} value={selectedClub.description} />
+      )}
+      <InfoItem icon={<CalendarDays />} value={createdAt} />
+    </Card>
+  );
+};
+
+const ClubSelect: FC<ClubSelectProps> = ({
+  clubs,
+  selectedClub,
+  handleChange,
+}) => {
+  const sortedClubs = [...clubs].sort((a, b) =>
+    a.id === selectedClub.id ? -1 : b.id === selectedClub.id ? 1 : 0,
+  );
+
+  return (
+    <Select onValueChange={(newValue) => handleChange(newValue)}>
+      <SelectTrigger style={{ border: 'none' }} className="w-full">
+        <SelectValue placeholder={selectedClub.name} />
+      </SelectTrigger>
+      <SelectContent>{sortedClubs.map(SelectItemIteratee)}</SelectContent>
+    </Select>
+  );
+};
+
+const SelectItemIteratee = (props: DatabaseClub) => {
+  return <SelectItem value={props.id}>{props.name}</SelectItem>;
+};
+
+type ClubSelectProps = {
+  clubs: DatabaseClub[];
+  selectedClub: DatabaseClub;
+  handleChange: (_arg: string) => void;
 };
 
 export default Main;
