d8049de5278443b313326bdbafc0c9e0ecdefafc
sukalov <matvey10@gmail.com>
2024-06-27 20:39:39 +0300

    fix?: move checkUser function to a separate server action

--- src/lib/actions/check-if-user-is-new.ts
+++ src/lib/actions/check-if-user-is-new.ts
@@ -1,6 +1,9 @@
 'use server'
 
-export const checkUser = () => {
+import { RequestCookie } from "next/dist/compiled/@edge-runtime/cookies";
+import { cookies } from "next/headers";
+
+export const checkUser = async () => {
     let test: RequestCookie | undefined = cookies().get('show_new_player_toast');
     if (test) cookies().delete('show_new_player_toast');
     return test;
