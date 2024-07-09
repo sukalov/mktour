f13988bbf1f254bb2a0d55e87ce277a8d03ed1a6
Anatoly <yatskovanatoly@gmail.com>
2024-07-09 09:17:40 +0300

    Fix: tournaments list in classic Dashboard

--- src/app/club/dashboard/tournaments/page.tsx
+++ src/app/club/dashboard/tournaments/page.tsx
@@ -1,63 +1,8 @@
-'use client';
+import TournamentsList from '@/app/club/dashboard/tournaments/tournaments-list';
+import { validateRequest } from '@/lib/auth/lucia';
 
-import Loading from '@/app/loading';
-import Empty from '@/components/empty';
-import { useClubTournaments } from '@/components/hooks/query-hooks/use-club-tournaments';
-import { useUser } from '@/components/hooks/query-hooks/use-user';
-import SkeletonList from '@/components/skeleton-list';
-import {
-  Card,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
-import { DatabaseTournament } from '@/lib/db/schema/tournaments';
-import { useTranslations } from 'next-intl';
-import Link from 'next/link';
-import { FC } from 'react';
-
-const ClubDashboardTournaments: FC<{ userId: string }> = ({ userId }) => {
-  const user = useUser(userId);
-  if (!user || !user.data) return <Loading />;
-  const { data, isLoading, isError, failureReason } = useClubTournaments(
-    user.data.selected_club,
-  );
-
-  const t = useTranslations('Empty');
-  console.log(data);
-
-  if (isLoading) return <SkeletonList length={4} />;
-  if (!data || !data.length) return <Empty>{t('tournaments')}</Empty>;
-  if (isError) return <p className="w-full">{failureReason?.message}</p>;
-  return (
-    <div className="mb-2 flex flex-col gap-2">{data?.map(TournamentItem)}</div>
-  );
-};
-
-const TournamentItem = (props: DatabaseTournament) => {
-  const details = [props.type, props.format, props.date];
-
-  const description = details.map((detail, i) => {
-    const separator = i === details.length - 1 ? '' : '|';
-    return <span key={i}>{`${detail} ${separator}`}</span>;
-  });
-
-  return (
-    <Link
-      key={props.id}
-      href={`/tournament/${props.id}`}
-      className="flex w-full flex-col"
-    >
-      <Card>
-        <CardHeader>
-          <CardTitle>{props.title}</CardTitle>
-          <CardDescription className="flex gap-2">
-            {description}
-          </CardDescription>
-        </CardHeader>
-      </Card>
-    </Link>
-  );
-};
-
-export default ClubDashboardTournaments;
+export default async function ClubDashboardTournaments() {
+  const { user } = await validateRequest();
+  if (!user || !user.id) return null;
+  return <TournamentsList userId={user.id} />;
+}
---
+++ src/app/club/dashboard/tournaments/tournaments-list.tsx
@@ -0,0 +1,56 @@
+'use client';
+
+import Loading from '@/app/loading';
+import { useClubTournaments } from '@/components/hooks/query-hooks/use-club-tournaments';
+import { useUser } from '@/components/hooks/query-hooks/use-user';
+import SkeletonList from '@/components/skeleton-list';
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { DatabaseTournament } from '@/lib/db/schema/tournaments';
+import Link from 'next/link';
+import { FC } from 'react';
+
+const TournamentsList: FC<{ userId: string }> = ({ userId }) => {
+  const user = useUser(userId);
+  if (!user || !user.data) return <Loading />;
+  const { data, isLoading, isError, failureReason } = useClubTournaments(
+    user.data.selected_club,
+  );
+  if (isLoading) return <SkeletonList length={4} />;
+  if (isError) return <p className="w-full">{failureReason?.message}</p>;
+  return (
+    <div className="mb-2 flex flex-col gap-2">{data?.map(TournamentItem)}</div>
+  );
+};
+
+const TournamentItem = (props: DatabaseTournament) => {
+  const details = [props.type, props.format, props.date];
+
+  const description = details.map((detail, i) => {
+    const separator = i === details.length - 1 ? '' : '|';
+    return <span key={i}>{`${detail} ${separator}`}</span>;
+  });
+
+  return (
+    <Link
+      key={props.id}
+      href={`/tournament/${props.id}`}
+      className="flex w-full flex-col"
+    >
+      <Card>
+        <CardHeader>
+          <CardTitle>{props.title}</CardTitle>
+          <CardDescription className="flex gap-2">
+            {description}
+          </CardDescription>
+        </CardHeader>
+      </Card>
+    </Link>
+  );
+};
+
+export default TournamentsList;
