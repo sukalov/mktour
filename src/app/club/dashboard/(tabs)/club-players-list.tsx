52e23d494cba66ef93b9d6d3648f2c5af88e2317
Anatoly <yatskovanatoly@gmail.com>
2024-08-02 18:12:03 +0300

    Ref: club players list + intl

--- src/app/club/dashboard/(tabs)/club-players-list.tsx
+++ src/app/club/dashboard/(tabs)/club-players-list.tsx
@@ -5,7 +5,7 @@ import Empty from '@/components/empty';
 import { useClubPlayers } from '@/components/hooks/query-hooks/use-club-players';
 import SkeletonList from '@/components/skeleton-list';
 import { Card } from '@/components/ui/card';
-import { useTranslations } from 'next-intl';
+import { useFormatter, useTranslations } from 'next-intl';
 import { FC } from 'react';
 
 const ClubPlayersList: FC<ClubTabProps> = ({ selectedClub }) => {
@@ -17,38 +17,39 @@ const ClubPlayersList: FC<ClubTabProps> = ({ selectedClub }) => {
   if (players.data?.length < 1) return <Empty>{t('players')}</Empty>;
 
   return (
-    <div className="flex flex-col gap-2">
-      {players.data.map(({ nickname, rating, last_seen, id }) => {
-        const lastSeen = getFormmatedLastSeen(last_seen);
-        const realname = 'Real Name';
-        return (
-          <Card
-            key={id}
-            className="flex items-center justify-between truncate p-4"
-          >
-            <div className="flex flex-col truncate">
-              <span>{nickname}</span>
-              <span className="text-xs">{realname && `(${realname})`}</span>
-              <span className="text-xs text-muted-foreground">
-                last seen: {lastSeen}
-              </span>
-            </div>
-            <div className="text-2xl">{rating}</div>
-          </Card>
-        );
-      })}
-    </div>
+    <div className="flex flex-col gap-2">{players.data.map(PlayerItem)}</div>
   );
 };
 
-// FIXME !!! that's what INTL is for ffs !!!
-const getFormmatedLastSeen = (num: number | null) => {
-  if (!num || num < 1) {
-    return `today`;
-  }
-  if (num >= 1) {
-    return `${num} day${num > 1 && 's'} ago`;
-  }
+const PlayerItem = ({ nickname, rating, last_seen, id, realname }: PlayerProps) => {
+  const t = useTranslations()
+  const formatter = useFormatter();
+  const lastSeen = last_seen && last_seen > 0 
+    ? formatter.relativeTime(last_seen) 
+    : t('Player.never');
+  
+  return (
+    <Card key={id} className="flex items-center justify-between truncate p-4">
+      <div className="flex flex-col truncate">
+        <span>{nickname}</span>
+        <span className="text-xs">{realname && `(${realname})`}</span>
+        <span className="text-xs text-muted-foreground">
+          {t('Player.last seen') + lastSeen}
+        </span>
+      </div>
+      <div className="text-2xl">{rating}</div>
+    </Card>
+  );
+};
+
+type PlayerProps = {
+  id: string;
+  nickname: string;
+  realname: string | null;
+  user_id: string | null;
+  rating: number | null;
+  club_id: string;
+  last_seen: number | null;
 };
 
 export default ClubPlayersList;
