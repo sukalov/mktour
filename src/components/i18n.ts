1cdceb722d281ae43aa355b462e076a9241fe298
sukalov <matvey10@gmail.com>
2024-08-08 18:33:06 +0300

    ref: move messages up (from lib)

--- src/components/i18n.ts
+++ src/components/i18n.ts
@@ -8,7 +8,7 @@ export default getRequestConfig(async () => {
 
   return {
     locale,
-    messages: (await import(`./messages/${locale}.json`)).default,
+    messages: (await import(`../messages/${locale}.json`)).default,
   };
 });
 
