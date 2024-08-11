a71ed3c12713e1b720d6c5ceb90f0c1c5eafd768
Anatoly <yatskovanatoly@gmail.com>
2024-08-11 20:47:36 +0300

    Fix: forward-to-empty club refactor & styling

--- src/app/club/create/forward-to-empty-club.tsx
+++ src/app/club/create/forward-to-empty-club.tsx
@@ -2,7 +2,7 @@
 
 import { forwardAction } from '@/app/club/create/forward-action';
 import { Button } from '@/components/ui/button';
-import { Card, CardContent } from '@/components/ui/card';
+import { Card } from '@/components/ui/card';
 import { DatabaseClub } from '@/lib/db/schema/tournaments';
 import { ArrowRight } from 'lucide-react';
 import { useTranslations } from 'next-intl';
@@ -14,25 +14,23 @@ export default function ForwardToEmpryClub({
   club: DatabaseClub;
   userId: string;
 }) {
-  const t = useTranslations('NewClubForm.ForwardToEmpty')
+  const t = useTranslations('NewClubForm.ForwardToEmpty');
   return (
-    <div className="p-4">
-      <Card className="mx-auto max-w-[min(600px,98%)] border-none shadow-none sm:border-solid sm:shadow-sm">
-        <CardContent className="flex flex-col gap-4 p-4 pt-2 sm:p-8">
-          <p>
-            {t('first message')}&nbsp;
-            <span className='font-bold'>{club.name}</span>{t('second message')}
-          </p>
-          <Button
-            className="group"
-            onClick={async () =>
-              await forwardAction({ clubId: club.id, userId })
-            }
-          >
-            {t('button text')}&nbsp;{club.name}&nbsp;
-            <ArrowRight className="transition-all duration-200 group-hover:translate-x-1" />
-          </Button>
-        </CardContent>
+    <div className="p-4 py-2">
+      <Card className="mx-auto flex max-w-[min(600px,98%)] flex-col gap-4 border-none shadow-none sm:border-solid sm:p-8 sm:shadow-sm">
+        <div>
+          {t.rich('message', {
+            bold: (club) => <div className="font-bold">{club}</div>,
+            club: club.name,
+          })}
+        </div>
+        <Button
+          className="group py-8"
+          onClick={async () => await forwardAction({ clubId: club.id, userId })}
+        >
+          <div>{`${t('button text')} ${club.name}`}</div>
+          <ArrowRight className="transition-all duration-200 group-hover:translate-x-1" />
+        </Button>
       </Card>
     </div>
   );
