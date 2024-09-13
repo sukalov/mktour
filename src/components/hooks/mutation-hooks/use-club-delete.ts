fd236b83e753e400efe3440692316e3845def1b0
sukalov <matvey10@gmail.com>
2024-09-13 21:36:37 +0300

    feat: better error message when deleting last club

--- src/components/hooks/mutation-hooks/use-club-delete.ts
+++ src/components/hooks/mutation-hooks/use-club-delete.ts
@@ -1,8 +1,10 @@
 import { deleteClub } from '@/lib/actions/club-managing';
 import { QueryClient, useMutation } from '@tanstack/react-query';
+import { useTranslations } from 'next-intl';
 import { toast } from 'sonner';
 
 export default function useDeleteClubMutation(queryClient: QueryClient) {
+  const t = useTranslations('Toasts');
   return useMutation({
     mutationFn: deleteClub,
     onSuccess: (_error, { id, userId }) => {
@@ -11,6 +13,12 @@ export default function useDeleteClubMutation(queryClient: QueryClient) {
       queryClient.invalidateQueries({ queryKey: [userId, 'user', 'clubs'] });
       queryClient.invalidateQueries({ queryKey: [userId, 'user', 'profile'] });
     },
-    onError: () => toast.error('sorry! server error happened'),
+    onError: (error) => {
+      if (error.message === 'ZERO_CLUBS') {
+        toast.error(t('zero clubs error', { id: 'zeroClubsError' }));
+      } else {
+        toast.error('sorry! server error happened', { id: 'serverError' });
+      }
+    },
   });
 }
