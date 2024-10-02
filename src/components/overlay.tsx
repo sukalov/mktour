7131fac37e8c8220375b507af95f0c3d4cc9195c
Anatoly <yatskovanatoly@gmail.com>
2024-10-02 03:35:07 +0300

    Ref: overlay simplified

--- src/components/overlay.tsx
+++ src/components/overlay.tsx
@@ -1,16 +1,9 @@
-import { FC, PropsWithChildren } from 'react';
+import { FC } from 'react';
 
-const Overlay: FC<{ open: boolean } & PropsWithChildren> = ({
-  open,
-  children,
-}) => {
-  return (
-    <div
-      className={`pointer-events-none absolute bottom-0 top-0 z-50 h-full w-full ${open ? 'opacity-100' : 'opacity-0'} backdrop-blur-[1px] backdrop-brightness-50 transition-opacity delay-75 duration-300`}
-    >
-      {children}
-    </div>
-  );
-};
+const Overlay: FC<{ open: boolean }> = ({ open }) => (
+  <div
+    className={`${open ? 'pointer-events-auto' : 'pointer-events-none'} absolute bottom-0 top-0 z-50 h-full w-full ${open ? 'opacity-100' : 'opacity-0'} backdrop-blur-[1px] backdrop-brightness-50 transition-opacity delay-75 duration-300`}
+  />
+);
 
 export default Overlay;
