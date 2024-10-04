465b07080361ba4c958670f796b91d1059cfdceb
sukalov <matvey10@gmail.com>
2024-10-04 13:15:38 +0300

    chore: ip = localhost in dev urls

--- src/lib/config/urls.ts
+++ src/lib/config/urls.ts
@@ -1,12 +1,12 @@
 export const BASE_URL =
   process.env.NODE_ENV === 'production'
     ? process.env.NEXT_PUBLIC_BASE_URL
-    : 'http://192.168.88.89:3000';
+    : 'http://localhost:3000';
 
 export const SOCKET_URL =
   process.env.NODE_ENV === 'production'
     ? process.env.NEXT_PUBLIC_SOCKET_URL
-    : 'ws://192.168.88.89:7070';
+    : 'ws://localhost:7070';
 
 export const DATABASE_URL =
   process.env.OFFLINE === 'true'
