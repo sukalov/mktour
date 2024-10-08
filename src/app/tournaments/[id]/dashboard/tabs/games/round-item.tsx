1cf4cabe4236b59e1d348b290ebe3eabb19a72a5
sukalov <matvey10@gmail.com>
2024-10-08 03:59:41 +0300

    feat: button "new round" when all round results added

--- src/app/tournaments/[id]/dashboard/tabs/games/round-item.tsx
+++ src/app/tournaments/[id]/dashboard/tabs/games/round-item.tsx
@@ -2,10 +2,13 @@
 import { DashboardContext } from '@/app/tournaments/[id]/dashboard/dashboard-context';
 import GameItemCompact from '@/app/tournaments/[id]/dashboard/tabs/games/game-item-compact';
 import Center from '@/components/center';
+import { useTournamentInfo } from '@/components/hooks/query-hooks/use-tournament-info';
 import { useTournamentRoundGames } from '@/components/hooks/query-hooks/use-tournament-round-games';
 import SkeletonList from '@/components/skeleton-list';
+import { Button } from '@/components/ui/button';
 import { GameModel } from '@/types/tournaments';
 import { AnimatePresence, motion } from 'framer-motion';
+import { useTranslations } from 'next-intl';
 import { usePathname } from 'next/navigation';
 import { FC, useContext } from 'react';
 
@@ -19,10 +22,12 @@ const RoundItem: FC<RoundItemProps> = ({ roundNumber }) => {
     tournamentId,
     roundNumber,
   });
+  const info = useTournamentInfo(tournamentId);
+  const t = useTranslations('Tournament.Round')
 
   const { escapedItemId } = useContext(DashboardContext);
 
-  if (isLoading)
+  if (isLoading || !info.data)
     return (
       <div className="px-4">
         <SkeletonList length={8} height={16} />
@@ -34,28 +39,54 @@ const RoundItem: FC<RoundItemProps> = ({ roundNumber }) => {
     return Number(a.result !== null) - Number(b.result !== null);
   });
 
-  return (
-    <div className="flex w-full flex-col gap-2 px-4">
-      <AnimatePresence>
-        {sortedRound.map((game, index) => {
-          let escaped = game.id === escapedItemId;
+  const ongoingGames = round.reduce(
+    (acc, current) => (current.result === null ? acc + 1 : acc),
+    0,
+  );
 
-          return (
-            <motion.div
-              key={game.id}
-              layout
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-              transition={{ duration: 0.3 }}
-              className={`${escaped && 'z-50'}`}
-            >
-              <GamesIteratee key={index} {...game} />
-            </motion.div>
-          );
-        })}
-      </AnimatePresence>
-    </div>
+  return (
+    <>
+      {/* {roundNumber === info.data.tournament.ongoing_round &&
+        ongoingGames === 0 && (
+          <div className="flex w-full flex-col gap-2 px-4 py-2">
+            <Button>make new round</Button>
+          </div>
+        )} */}
+      <div className="flex w-full flex-col gap-2 px-4 pt-2">
+        <AnimatePresence>
+          {roundNumber === info.data.tournament.ongoing_round &&
+            ongoingGames === 0 && (
+              <motion.div
+                layout
+                initial={{ opacity: 0, scale: 0.9 }}
+                animate={{ opacity: 1, scale: 1 }}
+                exit={{ opacity: 0, scale: 0.9 }}
+                transition={{ duration: 0.3 }}
+                className='pb-2'
+              >
+                <Button className="w-full">{t('new round button')}</Button>
+              </motion.div>
+            )}
+          {sortedRound.map((game, index) => {
+            let escaped = game.id === escapedItemId;
+
+            return (
+              <motion.div
+                key={game.id}
+                layout
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 0.3 }}
+                className={`${escaped && 'z-50'}`}
+              >
+                <GamesIteratee key={index} {...game} />
+              </motion.div>
+            );
+          })}
+        </AnimatePresence>
+      </div>
+    </>
   );
 };
 
