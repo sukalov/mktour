2ea0b84d1587e5198983e02f5d74e00a2b7cbb0b
sukalov <matvey10@gmail.com>
2024-10-10 03:19:50 +0300

    build: update drizzle-kit to v0.25 and fix config to match new config schema

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,10 @@
 import { DATABASE_AUTH_TOKEN, DATABASE_URL } from '@/lib/config/non-next-urls';
-import type { Config } from 'drizzle-kit';
-import { defineConfig } from 'drizzle-kit';
+import { Config, defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
   schema: './src/lib/db/schema',
   out: './src/lib/db/migrations',
-  dialect: 'sqlite',
-  driver: 'turso',
+  dialect: 'turso',
   dbCredentials: {
     url: DATABASE_URL,
     authToken: DATABASE_AUTH_TOKEN,
