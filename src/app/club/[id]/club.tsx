fe674371c92b6194b5396ce6f43efa1117a00625
Anatoly <yatskovanatoly@gmail.com>
2025-01-07 07:46:57 +0300

    Styles: Club list and club card

--- src/app/club/[id]/club.tsx
+++ src/app/club/[id]/club.tsx
@@ -1,52 +1,60 @@
 'use client';
-
 import ClubDashboardTournaments from '@/app/club/dashboard/(tabs)/tournaments-list';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
-import { motion } from 'framer-motion';
 import { useLocale, useTranslations } from 'next-intl';
 import Link from 'next/link';
-import { FC } from 'react';
+import { FC, PropsWithChildren } from 'react';
 
 const ClubPage: FC<ClubProps> = ({ club }) => {
+  const t = useTranslations();
+
+  return (
+    <div className="space-y-4 p-4">
+      <ClubCard club={club}>
+        {club.lichess_team && (
+          <Link
+            href={`https://lichess.org/team/${club.lichess_team}`}
+            target="_blank"
+          >
+            {t('Club.Page.lichess team')}
+          </Link>
+        )}
+      </ClubCard>
+      <div className="flex flex-col gap-2">
+        <p>{t('Club.Page.tournaments')}</p>
+        <ClubDashboardTournaments selectedClub={club.id} userId="" />
+      </div>
+    </div>
+  );
+};
+
+export const ClubCard: FC<PropsWithChildren & ClubProps> = ({
+  club,
+  children,
+}) => {
   const locale = useLocale();
   const t = useTranslations();
 
   return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-      className="space-y-4 p-4"
-    >
-      <Card key={club.id} className="shadow-lg">
-        <CardHeader>
-          <CardTitle>{club.name}</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <p className="text-sm">
-            {club.description || t('Club.Page.no description')}
-          </p>
-          {club.lichess_team && (
-            <Link
-              href={`https://lichess.org/team/${club.lichess_team}`}
-              target="_blank"
-            >
-              t('Club.Page.lichess team')
-            </Link>
-          )}
-          <p className="mt-2 text-xs text-muted-foreground">
-            {club.created_at &&
-              t('Club.Page.createdAt', {
-                date: club.created_at!.toLocaleDateString(locale, {
-                  dateStyle: 'long',
-                }),
-              })}
-          </p>
-        </CardContent>
-      </Card>
-      <p>{t('Club.Page.tournaments')}</p>
-      <ClubDashboardTournaments selectedClub={club.id} userId="" />
-    </motion.div>
+    <Card className="shadow-lg">
+      <CardHeader>
+        <CardTitle>{club.name}</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <p className="text-sm">
+          {club.description || t('Club.Page.no description')}
+        </p>
+        {children}
+        <p className="text-muted-foreground mt-2 text-xs">
+          {club.created_at &&
+            t('Club.Page.createdAt', {
+              date: club.created_at!.toLocaleDateString(locale, {
+                dateStyle: 'long',
+              }),
+            })}
+        </p>
+      </CardContent>
+    </Card>
   );
 };
 
--- src/app/club/all/clubs-list.tsx
+++ src/app/club/all/clubs-list.tsx
@@ -1,28 +1,18 @@
 'use client';
 
-import { Card, CardFooter } from '@/components/ui/card';
+import { ClubCard } from '@/app/club/[id]/club';
 import { DatabaseClub } from '@/lib/db/schema/tournaments';
-import { useFormatter } from 'next-intl';
 import Link from 'next/link';
 import { FC } from 'react';
 
-const ClubsList: FC<{ clubs: DatabaseClub[] }> = ({ clubs }) =>
-  clubs.map(ClubItem);
-
-const ClubItem = (club: DatabaseClub) => {
-  const format = useFormatter();
-
-  return (
-    <Card key={club.id} className="p-4">
-      <Link href={club.id}>
-        {club.name}
-        <CardFooter>{club.description}</CardFooter>
-        <div className="text-muted-foreground">
-          {format.dateTime(club.created_at!, { dateStyle: 'medium' })}
-        </div>
-      </Link>
-    </Card>
-  );
+const ClubsList: FC<{ clubs: DatabaseClub[] }> = ({ clubs }) => {
+  return clubs.map((club) => <ClubItemIteratee key={club.id} {...club} />);
 };
+
+const ClubItemIteratee = (club: DatabaseClub) => (
+  <Link key={club.id} href={`/club/${club.id}`}>
+    <ClubCard club={club} />
+  </Link>
+);
 
 export default ClubsList;
