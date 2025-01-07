eff49cedff1c1b19fb44294c5cf13efe2626daae
Anatoly <yatskovanatoly@gmail.com>
2025-01-07 05:24:15 +0300

    Style: layout bottom margin

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ async function RootLayout({ children }: PropsWithChildren) {
                 <ReactQueryProvider>
                   <NavWrapper />
                   <ViewTransitions>
-                    <div className="pt-14">{children}</div>
+                    <div className="pt-14 mb-40">{children}</div>
                   </ViewTransitions>
                   <Analytics />
                   <SpeedInsights />
