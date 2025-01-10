e3c4f74735c5f201c2f0b648c499b5ae6514b899
Anatoly <yatskovanatoly@gmail.com>
2025-01-10 07:41:29 +0300

    Experimental: handle Club Dashboard tab change on swipe

--- src/app/club/dashboard/dashboard.tsx
+++ src/app/club/dashboard/dashboard.tsx
@@ -10,31 +10,53 @@ import ClubDashboardTabList from '@/app/club/dashboard/dashboard-tab-list';
 import Loading from '@/app/loading';
 import Empty from '@/components/empty';
 import { useUser } from '@/components/hooks/query-hooks/use-user';
+import SwipeHandlerProvider from '@/components/swipe-handler-provider';
 import { useTranslations } from 'next-intl';
-import { FC, useState } from 'react';
+import { Dispatch, FC, SetStateAction, useState } from 'react';
+
 export default function Dashboard({ userId }: { userId: string }) {
   const t = useTranslations('Club.Dashboard');
   const { data, isLoading } = useUser(userId);
   const [tab, setTab] = useState<ClubDashboardTab>('main');
   const ActiveTab: FC<ClubTabProps> = tabMap[tab];
+  const tabs = Object.keys(tabMap) as ClubDashboardTab[];
+  const indexOfTab = tabs.indexOf(tab);
 
   if (!data && isLoading) return <Loading />;
   if (!data) return <Empty>{t('no data')}</Empty>;
 
   return (
-    <div>
+    <SwipeHandlerProvider
+      handleSwipe={(dir) => handleSwipe(dir, indexOfTab, tabs, setTab)}
+    >
       <ClubDashboardTabList activeTab={tab} setTab={setTab} />
       <div className="pt-12">
         <div className="px-1">
           <ClubSelect user={data} />
         </div>
-        <div className="p-2 pt-2">
+        <div className="p-2 pt-2 pb-16">
           <ActiveTab selectedClub={data.selected_club} userId={userId} />
         </div>
       </div>
-    </div>
+    </SwipeHandlerProvider>
   );
 }
+
+const handleSwipe = (
+  direction: string,
+  indexOfTab: number,
+  tabs: ClubDashboardTab[],
+  setTab: Dispatch<SetStateAction<ClubDashboardTab>>,
+) => {
+  let newIndex;
+  if (direction === 'right') {
+    newIndex = indexOfTab > 0 ? indexOfTab - 1 : tabs.length - 1;
+  } else if (direction === 'left') {
+    newIndex = indexOfTab < tabs.length - 1 ? indexOfTab + 1 : 0;
+  } else return;
+
+  setTab(tabs[newIndex]);
+};
 
 export const tabMap: Record<ClubDashboardTab, FC<ClubTabProps>> = {
   main: ClubMain,
