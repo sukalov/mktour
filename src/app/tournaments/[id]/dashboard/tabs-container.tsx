c95fdf6865d2e371fa6d0be4f6e854453abdcd78
Anatoly <yatskovanatoly@gmail.com>
2025-01-10 07:32:03 +0300

    Chores: removed unnecessary helpers folder

--- src/app/tournaments/[id]/dashboard/tabs-container.tsx
+++ src/app/tournaments/[id]/dashboard/tabs-container.tsx
@@ -4,8 +4,6 @@ import {
   DashboardContextType,
 } from '@/app/tournaments/[id]/dashboard/dashboard-context';
 import tabs from '@/app/tournaments/[id]/dashboard/tabs';
-import handleSwipe from '@/components/helpers/handle-swipe';
-import SwipeDetector from '@/components/helpers/swipe-detector';
 import Overlay from '@/components/overlay';
 import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs-grid';
 import { useTranslations } from 'next-intl';
@@ -14,41 +12,32 @@ import { FC, useContext, useRef } from 'react';
 const TabsContainer: FC<TabProps> = ({ currentTab, setCurrentTab }) => {
   const value: DashboardContextType['currentTab'] = currentTab;
   const tabRef = useRef<HTMLDivElement>(null);
-  const indexOfTab = tabs?.findIndex((tab) => tab?.title === value);
   const t = useTranslations('Tournament.Tabs');
   const { selectedGameId } = useContext(DashboardContext);
 
-  const onSwipe = (direction: string) =>
-    handleSwipe(direction, indexOfTab, setCurrentTab);
-
   return (
-    <SwipeDetector
-      onSwipeLeft={() => onSwipe('<')}
-      onSwipeRight={() => onSwipe('>')}
+    <Tabs
+      defaultValue="main"
+      onValueChange={(value) =>
+        setCurrentTab(value as DashboardContextType['currentTab'])
+      }
+      value={value}
+      className={`relative h-10 w-full rounded-none`}
     >
-      <Tabs
-        defaultValue="main"
-        onValueChange={(value) =>
-          setCurrentTab(value as DashboardContextType['currentTab'])
-        }
-        value={value}
-        className={`relative h-10 w-full rounded-none`}
+      <Overlay open={!!selectedGameId} />
+      <TabsList
+        ref={tabRef}
+        className={`no-scrollbar h-full w-full justify-around overflow-scroll rounded-none md:justify-evenly`}
       >
-        <Overlay open={!!selectedGameId} />
-        <TabsList
-          ref={tabRef}
-          className={`no-scrollbar h-full w-full justify-around overflow-scroll rounded-none md:justify-evenly`}
-        >
-          {tabs.map((tab) => (
-            <div key={tab.title}>
-              <TabsTrigger className="w-full" value={tab.title}>
-                {t(tab.title)}
-              </TabsTrigger>
-            </div>
-          ))}
-        </TabsList>
-      </Tabs>
-    </SwipeDetector>
+        {tabs.map((tab) => (
+          <div key={tab.title}>
+            <TabsTrigger className="w-full" value={tab.title}>
+              {t(tab.title)}
+            </TabsTrigger>
+          </div>
+        ))}
+      </TabsList>
+    </Tabs>
   );
 };
 
---
+++ src/components/swipe-handler-provider.tsx
@@ -0,0 +1,75 @@
+'use client';
+
+import { FC, PropsWithChildren, TouchEvent, useRef } from 'react';
+
+const SwipeHandlerProvider: FC<SwipeDetectorProps & PropsWithChildren> = ({
+  handleSwipe,
+  children,
+}) => {
+  const touchStartX = useRef<number | null>(null);
+  const touchStartY = useRef<number | null>(null);
+  const touchEndX = useRef<number | null>(null);
+  const touchEndY = useRef<number | null>(null);
+
+  const SWIPE_THRESHOLD = 30; // Minimum distance for a valid swipe
+  const VERTICAL_FILTER_RATIO = 2; // Vertical movement must be less than half the horizontal movement
+
+  function handleTouchStart(event: TouchEvent<HTMLDivElement>) {
+    event.stopPropagation();
+    touchStartX.current = event.touches[0].clientX;
+    touchStartY.current = event.touches[0].clientY;
+  }
+
+  function handleTouchMove(event: TouchEvent<HTMLDivElement>) {
+    event.stopPropagation();
+    touchEndX.current = event.touches[0].clientX;
+    touchEndY.current = event.touches[0].clientY;
+  }
+
+  function handleTouchEnd() {
+    if (
+      touchStartX.current !== null &&
+      touchStartY.current !== null &&
+      touchEndX.current !== null &&
+      touchEndY.current !== null
+    ) {
+      const deltaX = touchEndX.current - touchStartX.current;
+      const deltaY = touchEndY.current - touchStartY.current;
+
+      // Check if the swipe is horizontal
+      if (
+        Math.abs(deltaX) > SWIPE_THRESHOLD &&
+        Math.abs(deltaY) < Math.abs(deltaX) / VERTICAL_FILTER_RATIO
+      ) {
+        if (deltaX > 0) {
+          handleSwipe('right');
+        } else {
+          handleSwipe('left');
+        }
+      }
+    }
+
+    // Reset touch positions
+    touchStartX.current = null;
+    touchStartY.current = null;
+    touchEndX.current = null;
+    touchEndY.current = null;
+  }
+
+  return (
+    <div
+      className="h-[calc(100dvh-4rem)] w-full"
+      onTouchStart={handleTouchStart}
+      onTouchMove={handleTouchMove}
+      onTouchEnd={handleTouchEnd}
+    >
+      {children}
+    </div>
+  );
+};
+
+interface SwipeDetectorProps {
+  handleSwipe: (dir: string) => void | null;
+}
+
+export default SwipeHandlerProvider;
