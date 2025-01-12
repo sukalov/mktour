4f4a8f46deb6f2b83e02355faa00c8672fe75797
Anatoly <yatskovanatoly@gmail.com>
2025-01-12 14:39:52 +0300

    Refactor: tournament groups to /my

--- src/app/tournaments/my/page.tsx
+++ src/app/tournaments/my/page.tsx
@@ -1,7 +1,11 @@
-import TournamentsContainer from '@/app/tournaments/tournament-container';
+import TournamentItemIteratee from '@/components/tournament-item';
 import { validateRequest } from '@/lib/auth/lucia';
-import getTournamentsToUserClubsQuery from '@/lib/db/queries/get-tournaments-to-user-clubs-query';
+import getTournamentsToUserClubsQuery, {
+  TournamentWithClub,
+} from '@/lib/db/queries/get-tournaments-to-user-clubs-query';
+import { Link } from 'lucide-react';
 import { redirect } from 'next/navigation';
+import { FC } from 'react';
 
 export default async function MyTournaments() {
   const { user } = await validateRequest();
@@ -10,7 +14,44 @@ export default async function MyTournaments() {
 
   return (
     <main className="m-4 flex flex-col items-center gap-4">
-      <TournamentsContainer props={tournaments} grouped />
+      <TournamentGroups props={tournaments} />
     </main>
   );
 }
+
+const TournamentGroups: FC<{ props: TournamentWithClub[] }> = ({ props }) => {
+  const groupedTournaments = getGroupedTournaments(props);
+
+  return Object.entries(groupedTournaments).map(
+    ([clubId, { clubName, tournaments }]) => {
+      const data = tournaments.map((tour) => tour.tournament);
+      return (
+        <div className="flex w-full flex-col gap-2" key={clubId}>
+          <Link href={`/club/${clubId}`}>
+            <h2 className="text-muted-foreground">{clubName}</h2>
+          </Link>
+          {data.map(TournamentItemIteratee)}
+        </div>
+      );
+    },
+  );
+};
+
+const getGroupedTournaments = (props: TournamentWithClub[]) =>
+  props.reduce(
+    (acc, curr) => {
+      const { id: clubId, name: clubName } = curr.club;
+      if (!acc[clubId]) {
+        acc[clubId] = {
+          clubName,
+          tournaments: [],
+        };
+      }
+      acc[clubId].tournaments.push(curr);
+      return acc;
+    },
+    {} as Record<
+      string,
+      { clubName: string; tournaments: TournamentWithClub[] }
+    >,
+  );
--- src/app/tournaments/tournament-container.tsx
+++ src/app/tournaments/tournament-container.tsx
@@ -1,54 +1,12 @@
 import TournamentItemIteratee from '@/components/tournament-item';
 import { TournamentWithClub } from '@/lib/db/queries/get-tournaments-to-user-clubs-query';
-import Link from 'next/link';
 import { FC } from 'react';
 
-const TournamentsContainer: FC<{
-  props: TournamentWithClub[];
-  grouped?: boolean;
-}> = ({ props, grouped }) => {
-  if (grouped) {
-    return <TournamentGroups props={props} />;
-  }
+const TournamentsContainer: FC<{ props: TournamentWithClub[] }> = ({
+  props,
+}) => {
   const data = props.map((prop) => prop.tournament);
   return <>{data.map(TournamentItemIteratee)}</>;
 };
-
-const TournamentGroups: FC<{ props: TournamentWithClub[] }> = ({ props }) => {
-  const groupedTournaments = getGroupedTournaments(props);
-
-  return Object.entries(groupedTournaments).map(
-    ([clubId, { clubName, tournaments }]) => {
-      const data = tournaments.map((tour) => tour.tournament);
-      return (
-        <div className="flex w-full flex-col gap-2" key={clubId}>
-          <Link href={`/club/${clubId}`}>
-            <h2 className="text-muted-foreground">{clubName}</h2>
-          </Link>
-          {data.map(TournamentItemIteratee)}
-        </div>
-      );
-    },
-  );
-};
-
-const getGroupedTournaments = (props: TournamentWithClub[]) =>
-  props.reduce(
-    (acc, curr) => {
-      const { id: clubId, name: clubName } = curr.club;
-      if (!acc[clubId]) {
-        acc[clubId] = {
-          clubName,
-          tournaments: [],
-        };
-      }
-      acc[clubId].tournaments.push(curr);
-      return acc;
-    },
-    {} as Record<
-      string,
-      { clubName: string; tournaments: TournamentWithClub[] }
-    >,
-  );
 
 export default TournamentsContainer;
