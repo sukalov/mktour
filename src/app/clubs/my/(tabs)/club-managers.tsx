388c204885f2b19d1f17e50fa9e88fbda0f546d9
sukalov <matvey10@gmail.com>
2025-01-19 20:42:56 +0300

    feat: ui improvements for dialogs and club-settings

--- src/app/clubs/my/(tabs)/club-managers.tsx
+++ src/app/clubs/my/(tabs)/club-managers.tsx
@@ -3,6 +3,7 @@
 import { useClubManagers } from '@/components/hooks/query-hooks/use-club-managers';
 import { Button } from '@/components/ui/button';
 import { Card } from '@/components/ui/card';
+import { Skeleton } from '@/components/ui/skeleton';
 import { ClubManagers } from '@/lib/actions/club-managing';
 import { UserPlus } from 'lucide-react';
 import { useTranslations } from 'next-intl';
@@ -10,19 +11,27 @@ import Link from 'next/link';
 import { FC } from 'react';
 
 const ClubManagersList: FC<{ id: string }> = ({ id }) => {
-  const { data } = useClubManagers(id);
+  const { data, isLoading } = useClubManagers(id);
+  // const [addingManager, setAddingManager] = useState(false);
   const t = useTranslations('Club');
+
   return (
     <div className="flex flex-col gap-2 px-6 sm:px-0">
       {t('managers list')}
-      <Card className="p-4">{data?.map(ManagersIteratee)}</Card>
+      <Card className="border-none shadow-none px-6 py-3 sm:border-solid sm:px-12 sm:shadow-2xs">
+        {isLoading ? (
+          <Skeleton className="h-12 w-full" />
+        ) : (
+          <ul className="list-disc">{data?.map(ManagersIteratee)}</ul>
+        )}
+      </Card>
       <Button
         size="icon"
         variant="outline"
         onClick={console.log}
         className="flex w-full gap-2"
       >
-        <UserPlus />
+        <UserPlus size={18} />
         {t('add manager')}
       </Button>
     </div>
@@ -30,7 +39,9 @@ const ClubManagersList: FC<{ id: string }> = ({ id }) => {
 };
 
 const ManagersIteratee = (manager: ClubManagers) => (
-  <ManagerItem key={manager.user?.id} manager={manager} />
+  <li className="not-last:pb-2" key={manager.user?.id}>
+    <ManagerItem manager={manager} />
+  </li>
 );
 
 const ManagerItem: FC<{ manager: ClubManagers }> = ({ manager }) => {
@@ -38,7 +49,11 @@ const ManagerItem: FC<{ manager: ClubManagers }> = ({ manager }) => {
 
   return (
     <Link href={`/user/${manager.user?.username}`} key={manager.user?.id}>
-      {manager.user?.username} ({t(manager.clubs_to_users!.status)})
+      {manager.user?.username}
+      &nbsp;
+      <span className="text-muted-foreground">
+        {t(manager.clubs_to_users!.status)}
+      </span>
     </Link>
   );
 };
