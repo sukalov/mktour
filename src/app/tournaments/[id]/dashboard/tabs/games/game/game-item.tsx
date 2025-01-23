a2216f7e224855ca3cefbc247552869981e67dfb
Anatoly <yatskovanatoly@gmail.com>
2025-01-23 19:57:37 +0300

    Feat: drawer for start tournament before entering results

--- src/app/tournaments/[id]/dashboard/tabs/games/game/game-item.tsx
+++ src/app/tournaments/[id]/dashboard/tabs/games/game/game-item.tsx
@@ -3,16 +3,33 @@ import Player from '@/app/tournaments/[id]/dashboard/tabs/games/game/player';
 import Result, {
   ResultProps,
 } from '@/app/tournaments/[id]/dashboard/tabs/games/game/result';
+import StartTournamentButton from '@/app/tournaments/[id]/dashboard/tabs/main/start-tournament-button';
+import FormattedMessage from '@/components/formatted-message';
 import useTournamentSetGameResult from '@/components/hooks/mutation-hooks/use-tournament-set-game-result';
 import { useTournamentInfo } from '@/components/hooks/query-hooks/use-tournament-info';
 import useOutsideClick from '@/components/hooks/use-outside-click';
 import { Button } from '@/components/ui/button';
 import { Card } from '@/components/ui/card';
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerHeader,
+  DrawerTitle,
+} from '@/components/ui/drawer';
 import { Result as ResultModel } from '@/types/tournaments';
 import { useQueryClient } from '@tanstack/react-query';
 import { motion } from 'framer-motion';
 import { useParams } from 'next/navigation';
-import { FC, useCallback, useContext, useEffect, useRef } from 'react';
+import {
+  FC,
+  useCallback,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
 
 const GameItem: FC<GameProps> = ({
   id,
@@ -34,12 +51,15 @@ const GameItem: FC<GameProps> = ({
   const ref = useRef<HTMLDivElement>(null);
   const selected = selectedGameId === id;
   const muted = result && !selected;
+  const [open, setOpen] = useState(false);
+  const hasStarted = !!data?.tournament.started_at;
 
   const handleCardState = useCallback(
     (state: boolean) => {
-      setSelectedGameId(state ? id : null);
+      if (!hasStarted) setOpen(true);
+      else setSelectedGameId(state ? id : null);
     },
-    [id, setSelectedGameId],
+    [id, setSelectedGameId, hasStarted],
   );
 
   const handleMutate = (newResult: ResultModel) => {
@@ -68,55 +88,85 @@ const GameItem: FC<GameProps> = ({
     }
   }, [handleCardState, mutation.isSuccess]);
 
+  useEffect(() => setOpen(false), [hasStarted]);
+
   useOutsideClick(() => {
     if (selected) {
       handleCardState(false);
     }
   }, ref);
 
-  const disabled =
-    status !== 'organizer' ||
-    !!data?.tournament.closed_at ||
-    !data?.tournament.started_at;
+  const disabled = status !== 'organizer' || !!data?.tournament.closed_at;
 
   return (
-    <motion.div
-      key={id}
-      ref={ref}
-      className={`${disabled && 'pointer-events-none'} cursor-pointer rounded-lg shadow-lg md:w-fit ${
-        selected ? 'z-50' : 'z-0'
-      }`}
-      initial={{ scale: 1, y: 0 }}
-      animate={selected ? { scale: 1.05, y: -10 } : { scale: 1, y: 0 }}
-      transition={{ type: 'spring', bounce: 0.5 }}
-      onClick={() => handleCardState(true)}
-    >
-      <Card
-        className={`grid ${muted && 'opacity-50'} h-16 w-full grid-cols-3 items-center gap-2 border p-2 text-sm transition-all select-none ${!selected && 'pointer-events-none'} md:max-w-72`}
+    <>
+      <motion.div
+        key={id}
+        ref={ref}
+        className={`${disabled && 'pointer-events-none'} cursor-pointer rounded-lg shadow-lg md:w-fit ${
+          selected ? 'z-50' : 'z-0'
+        }`}
+        initial={{ scale: 1, y: 0 }}
+        animate={selected ? { scale: 1.05, y: -10 } : { scale: 1, y: 0 }}
+        transition={{ type: 'spring', bounce: 0.5 }}
+        onClick={() => handleCardState(true)}
       >
-        <Player
-          isWinner={result === '1-0'}
-          handleMutate={() => handleMutate('1-0')}
-          selected={selected}
-          nickname={playerLeft.white_nickname}
-          position={{ justify: 'justify-self-start', text: 'text-left' }}
-        />
-        <Button
-          variant="ghost"
-          onClick={() => handleMutate('1/2-1/2')}
-          className={`mx-4 flex h-full w-full min-w-16 grow gap-2 justify-self-center rounded-sm p-1 px-2 select-none ${selected && draw && 'underline underline-offset-4'}`}
+        <Card
+          className={`grid ${muted && 'opacity-50'} h-16 w-full grid-cols-3 items-center gap-2 border p-2 text-sm transition-all select-none ${!selected && 'pointer-events-none'} md:max-w-72`}
         >
-          <Result {...resultProps} />
-        </Button>
-        <Player
-          isWinner={result === '0-1'}
-          handleMutate={() => handleMutate('0-1')}
-          selected={selected}
-          nickname={playerRight.black_nickname}
-          position={{ justify: 'justify-self-end', text: 'text-right' }}
-        />
-      </Card>
-    </motion.div>
+          <Player
+            isWinner={result === '1-0'}
+            handleMutate={() => handleMutate('1-0')}
+            selected={selected}
+            nickname={playerLeft.white_nickname}
+            position={{ justify: 'justify-self-start', text: 'text-left' }}
+          />
+          <Button
+            variant="ghost"
+            onClick={() => handleMutate('1/2-1/2')}
+            className={`mx-4 flex h-full w-full min-w-16 grow gap-2 justify-self-center rounded-sm p-1 px-2 select-none ${selected && draw && 'underline underline-offset-4'}`}
+          >
+            <Result {...resultProps} />
+          </Button>
+          <Player
+            isWinner={result === '0-1'}
+            handleMutate={() => handleMutate('0-1')}
+            selected={selected}
+            nickname={playerRight.black_nickname}
+            position={{ justify: 'justify-self-end', text: 'text-right' }}
+          />
+        </Card>
+      </motion.div>
+      <StartTournamentDrawer open={open} setOpen={setOpen} />
+    </>
+  );
+};
+
+const StartTournamentDrawer: FC<{
+  open: boolean;
+  setOpen: (arg: boolean) => void;
+}> = ({ open, setOpen }) => {
+  return (
+    <Drawer open={open} onClose={() => setOpen(false)}>
+      <DrawerContent>
+        <DrawerHeader className="text-left">
+          <DrawerTitle>
+            <FormattedMessage id="Tournament.Round.start tournament.title" />
+          </DrawerTitle>
+          <DrawerDescription>
+            <FormattedMessage id="Tournament.Round.start tournament.description" />
+          </DrawerDescription>
+        </DrawerHeader>
+        <div className="flex w-full flex-col gap-4 p-4 pt-0">
+          <StartTournamentButton />
+          <DrawerClose asChild>
+            <Button size="lg" variant="outline">
+              <FormattedMessage id="Common.cancel" />
+            </Button>
+          </DrawerClose>
+        </div>
+      </DrawerContent>
+    </Drawer>
   );
 };
 
