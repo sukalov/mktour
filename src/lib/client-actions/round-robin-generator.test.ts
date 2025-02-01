17932c5c71a5f8eede9545efca6f8753997d1c9f
Mark <strategy155@gmail.com>
2025-02-01 18:37:03 +0100

    changed test schema WIP now user and tournament separated

--- src/lib/client-actions/round-robin-generator.test.ts
+++ src/lib/client-actions/round-robin-generator.test.ts
@@ -1,30 +1,58 @@
 import { expect, mock, test } from "bun:test";
 
+import { DatabaseUser } from "@/lib/db/schema/auth";
 import { newid } from "@/lib/utils";
 import { PlayerModel } from "@/types/tournaments";
 import { faker } from "@faker-js/faker";
 
+const INITIAL_WINS = 0;
 
 const COLOUR_INDEX_FAKEOPTS = {
   min: -10,
   max: 10
-}
+};
 
-const generatePlayerModel = mock(
+const RATING_FAKEOPTS = {
+  min:500,
+  max:3000
+};
+
+const generateDatabasePlayer = mock(
   () => {
     const randomId = newid();
     const randomNickname = faker.internet.username();
-    const randomWins = faker.number.int();
-    const randomDraws = faker.number.int();
-    const randomLosses = faker.number.int();
-    const randomColourIndex = faker.number.int(COLOUR_INDEX_FAKEOPTS);
+    const randomRealName = faker.person.fullName();
+    const randomRating = faker.number.float(RATING_FAKEOPTS);
+    const randomEmail = faker.internet.email();
+    const randomClubName = faker.company.name();
+    const randomCreationDate = faker.date.anytime();
+    const randomUser: DatabaseUser = {
+      id: randomId,
+      username: randomNickname,
+      name: randomRealName,
+      email: randomEmail, 
+      rating: randomRating,
+      selected_club: randomClubName,
+      created_at: randomCreationDate
+    };
+    return randomUser;
+  }
+)
+
+const generatePlayerModel = mock(
+  () => {
+    const randomUser = generateDatabasePlayer();
     const randomPlayer: PlayerModel = {
       id: randomId,
       nickname: randomNickname, 
-      wins: randomWins,
-      draws: randomDraws,
-      losses: randomLosses,
-      color_index: randomColourIndex
+      wins: 0,
+      draws: 0,
+      losses: 0,
+      color_index: 0,
+      realname: randomRealName,
+      rating: randomRating,
+      exited: null,
+      place: null,
     }
     
     
