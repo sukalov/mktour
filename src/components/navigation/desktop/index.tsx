fa08865eb1998d0a9a34d5051b3817f7bce8de84
sukalov <matvey10@gmail.com>
2025-02-02 16:23:41 +0300

    fix: fix styles and layout for globalsearch

--- src/components/navigation/desktop/index.tsx
+++ src/components/navigation/desktop/index.tsx
@@ -3,12 +3,14 @@ import LocaleSwitcher from '@/components/locale-switcher';
 import { NavMenuProps } from '@/components/navigation';
 import NavigationMenuContainer from '@/components/navigation/desktop/navigation-menu';
 import ModeToggler from '@/components/navigation/mode-toggler';
+import GlobalSearch from '@/components/navigation/search';
 import { FC } from 'react';
 
 const Desktop: FC<NavMenuProps> = ({ user }) => {
   return (
     <div className="flex w-full justify-end">
       <NavigationMenuContainer />
+      <GlobalSearch />
       <div className="flex items-center">
         <AuthButton user={user} />
         <LocaleSwitcher />
--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -38,14 +38,14 @@ const DialogContent = React.forwardRef<
     <DialogPrimitive.Content
       ref={ref}
       className={cn(
-        'bg-background data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 fixed top-[50%] left-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border p-6 shadow-lg duration-200 sm:rounded-lg',
+        'bg-background data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 fixed top-0 left-[50%] z-50 grid w-full max-w-2xl translate-x-[-50%] gap-4 border p-6 shadow-lg duration-200 sm:top-[50%] sm:max-w-lg sm:translate-y-[-50%] sm:rounded-lg',
         className,
       )}
       {...props}
     >
       {children}
       <DialogPrimitive.Close className="ring-offset-background focus:ring-ring data-[state=open]:bg-accent data-[state=open]:text-muted-foreground absolute top-4 right-4 rounded-sm opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-none disabled:pointer-events-none">
-        <X className="h-4 w-4" />
+        <X className="h-5 w-5" />
         <span className="sr-only">Close</span>
       </DialogPrimitive.Close>
     </DialogPrimitive.Content>
@@ -118,5 +118,6 @@ export {
   DialogOverlay,
   DialogPortal,
   DialogTitle,
-  DialogTrigger,
+  DialogTrigger
 };
+
