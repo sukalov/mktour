16c37d29498a7269e2af722d335ea1b636cb755e
tol ya <123395646+yatskovanatoly@users.noreply.github.com>
2025-02-07 19:51:10 +0300

    Style: PWA theme

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -6,6 +6,8 @@ export default function manifest(): MetadataRoute.Manifest {
     short_name: 'mktour',
     start_url: '/',
     display: 'standalone',
+    background_color: '#ffffff',
+    theme_color: '#000000',
     icons: [
       {
         src: '/icon.png',
