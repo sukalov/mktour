f805f03d3277548614011ebc0d546cedf881f565
tol ya <123395646+yatskovanatoly@users.noreply.github.com>
2025-02-07 19:35:50 +0300

    Chores: PWA naming

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -2,8 +2,8 @@ import type { MetadataRoute } from 'next';
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: 'mktour PWA',
-    short_name: 'mktourPWA',
+    name: 'mktour',
+    short_name: 'mktour',
     start_url: '/',
     display: 'standalone',
     icons: [
