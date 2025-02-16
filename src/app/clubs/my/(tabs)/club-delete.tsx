997a4cb7bb337c277d0a8713d4768b5ed4f4f230
Anatoly <yatskovanatoly@gmail.com>
2025-02-17 02:59:43 +0300

    Ref: comboModal for delete club confirmation

--- src/app/clubs/my/(tabs)/club-delete.tsx
+++ src/app/clubs/my/(tabs)/club-delete.tsx
@@ -2,90 +2,38 @@
 
 import DeleteConfirmationForm from '@/app/clubs/my/(tabs)/club-delete-form';
 import { Button } from '@/components/ui/button';
-import { Card, CardContent, CardHeader } from '@/components/ui/card';
-import {
-  Dialog,
-  DialogClose,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from '@/components/ui/dialog';
-import {
-  Drawer,
-  DrawerClose,
-  DrawerContent,
-  DrawerDescription,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerTitle,
-  DrawerTrigger,
-} from '@/components/ui/drawer';
+import ComboModal from '@/components/ui/combo-modal';
 import { useTranslations } from 'next-intl';
 import * as React from 'react';
-import { useMediaQuery } from 'react-responsive';
 
 export default function ClubDelete({ id, userId }: ClubDeleteProps) {
   const [open, setOpen] = React.useState(false);
-  const isDesktop = useMediaQuery({ minWidth: 768 });
   const t = useTranslations('Club.Settings');
-  if (isDesktop) {
-    return (
-      <Card className="border-none shadow-none sm:border-solid sm:shadow-2xs">
-        <CardHeader>{t('danger zone')}</CardHeader>
-        <CardContent className="flex flex-col gap-4">
-          <Dialog open={open} onOpenChange={setOpen}>
-            <DialogTrigger asChild>
-              <Button variant="destructive">{t('delete club')}</Button>
-            </DialogTrigger>
-            <DialogContent className="sm:max-w-[425px]">
-              <DialogHeader>
-                <DialogTitle>{t('confirmation title')}</DialogTitle>
-                <DialogDescription>{t('confirmation body')}</DialogDescription>
-              </DialogHeader>
-              <DeleteConfirmationForm
-                id={id}
-                userId={userId}
-                setOpenAction={setOpen}
-              />
-              <DialogClose asChild>
-                <Button variant="outline">{t('cancel')}</Button>
-              </DialogClose>
-            </DialogContent>
-          </Dialog>
-        </CardContent>
-      </Card>
-    );
-  }
 
   return (
-    <Card className="border-none shadow-none sm:border-solid sm:shadow-2xs">
-      <CardHeader>{t('danger zone')}</CardHeader>
-      <CardContent className="flex flex-col gap-4">
-        <Drawer open={open} onOpenChange={setOpen}>
-          <DrawerTrigger asChild>
-            <Button variant="destructive">{t('delete club')}</Button>
-          </DrawerTrigger>
-          <DrawerContent>
-            <DrawerHeader className="text-left">
-              <DrawerTitle>{t('confirmation title')}</DrawerTitle>
-              <DrawerDescription>{t('confirmation body')}</DrawerDescription>
-            </DrawerHeader>
-            <DeleteConfirmationForm
-              id={id}
-              userId={userId}
-              setOpenAction={setOpen}
-            />
-            <DrawerFooter className="pt-2">
-              <DrawerClose asChild>
-                <Button variant="outline">{t('cancel')}</Button>
-              </DrawerClose>
-            </DrawerFooter>
-          </DrawerContent>
-        </Drawer>
-      </CardContent>
-    </Card>
+    <ComboModal.Root open={open} onOpenChange={setOpen}>
+      <ComboModal.Trigger asChild className='max-sm:mx-6'>
+        <Button variant="destructive">
+          {t('delete club')}
+        </Button>
+      </ComboModal.Trigger>
+      <ComboModal.Content>
+        <ComboModal.Header>
+          <ComboModal.Title>{t('confirmation title')}</ComboModal.Title>
+          <ComboModal.Description>
+            {t('confirmation body')}
+          </ComboModal.Description>
+        </ComboModal.Header>
+        <DeleteConfirmationForm
+          id={id}
+          userId={userId}
+          setOpenAction={setOpen}
+        />
+        <ComboModal.Close asChild>
+          <Button variant="outline" className='w-full'>{t('cancel')}</Button>
+        </ComboModal.Close>
+      </ComboModal.Content>
+    </ComboModal.Root>
   );
 }
 
