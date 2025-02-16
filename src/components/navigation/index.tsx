df04f9a46b6e42d6e5598024e53894b8c7a0ff26
sukalov <matvey10@gmail.com>
2025-02-16 19:16:03 +0300

    fix: rendering globalsearch twice on cmd+k

--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -2,20 +2,17 @@
 
 import Desktop from '@/components/navigation/desktop';
 import Mobile from '@/components/navigation/mobile';
+import { MediaQueryContext } from '@/components/providers/media-query-context';
 import MktourNavbar from '@/components/ui/mktour-logo-navbar';
 import { User } from 'lucia';
-import { FC } from 'react';
+import { FC, useContext } from 'react';
 
 const Navigation: FC<NavMenuProps> = ({ user }) => {
+  const { isTablet } = useContext(MediaQueryContext);
   return (
     <nav className="bg-background fixed z-50 flex h-14 w-full items-center justify-between border-b p-4">
       <MktourNavbar />
-      <div className="md:hidden">
-        <Mobile user={user} />
-      </div>
-      <div className="hidden md:block">
-        <Desktop user={user} />
-      </div>
+      {isTablet ? <Mobile user={user} /> : <Desktop user={user} />}
     </nav>
   );
 };
