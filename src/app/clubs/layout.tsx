206cc28fec3b2c5fc5e6d931505eef1a4652e5a5
Anatoly <yatskovanatoly@gmail.com>
2025-03-17 02:47:16 +0300

    Rm test layout

--- src/app/clubs/layout.tsx
+++
@@ -1,7 +0,0 @@
-import { FC, PropsWithChildren } from 'react';
-
-const ClubsLayout: FC<PropsWithChildren> = async ({ children }) => (
-  <div className="mb-16 border-2 border-red-500">{children}</div>
-);
-
-export default ClubsLayout
