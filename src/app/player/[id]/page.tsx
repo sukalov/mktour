afb0555a058f58fe00c15f0394526cb4e5c78e99
Anatoly <yatskovanatoly@gmail.com>
2025-05-03 17:03:25 +0300

    Minor player/[id] styling

--- src/app/player/[id]/page.tsx
+++ src/app/player/[id]/page.tsx
@@ -15,58 +15,63 @@ export default async function PlayerPage(props: PlayerPageProps) {
   const { player, club } = await getPlayerQuery(params.id);
   if (!player || !club) notFound();
 
-  let status: StatusInClub | undefined | 'owner';
-  status = user ? await getStatus({ user, club }) : undefined;
+  const status: StatusInClub | undefined = user
+    ? await getStatus({ user, club })
+    : undefined;
 
   const isOwnPlayer = user && player.user_id === user.id;
   const canEdit = status === 'admin' || isOwnPlayer;
   const canClaim = status !== 'admin' && user && !player.user_id;
 
   return (
-    <div className="flex w-full flex-col gap-4 p-4 pt-2">
-      <div className="flex w-full items-center justify-between border-b-2 pb-2">
-        <span className="truncate text-2xl font-semibold text-wrap">
-          {player.nickname}
-        </span>
-        {user && (
-          <div className="text-muted-foreground flex self-end">
-            {canEdit && (
-              <>
-                <EditButton userId={user.id} player={player} />
-                <DeletePlayer userId={user.id} />
-              </>
-            )}
-            {canClaim && <ClaimPlayer userId={user.id} />}
-          </div>
-        )}
-      </div>
-      <div className="flex flex-col gap-2">
-        {player.realname && <span className="text-lg">{player.realname}</span>}
-        <span>
-          <FormattedMessage id="Player.rating" />
-          {': '}
-          {player.rating}
-        </span>
-        <p>
-          <FormattedMessage id="Player.club" />
-          {': '}
-          <Link className="mk-link" href={`/clubs/${player.club_id}`}>
-            {club.name}
-          </Link>
-        </p>
-        {player.user_id && (
-          <p>
-            <FormattedMessage id="Player.lichess" />
+    <div className="mk-container">
+      <div className="flex w-full flex-col gap-4">
+        <div className="flex w-full items-center px-2 pb-2 justify-between border-b-2">
+          <span className="truncate text-2xl font-semibold text-wrap">
+            {player.nickname}
+          </span>
+          {user && (
+            <div className="text-muted-foreground flex self-end">
+              {canEdit && (
+                <>
+                  <EditButton userId={user.id} player={player} />
+                  <DeletePlayer userId={user.id} />
+                </>
+              )}
+              {canClaim && <ClaimPlayer userId={user.id} />}
+            </div>
+          )}
+        </div>
+        <div className="flex flex-col gap-2 px-2">
+          {player.realname && (
+            <span className="text-lg">{player.realname}</span>
+          )}
+          <span>
+            <FormattedMessage id="Player.rating" />
             {': '}
-            <Link
-              href={`https://lichess.org/@/${player.nickname}`}
-              target="_blank"
-              className="mk-link"
-            >
-              {player.nickname}
+            {player.rating}
+          </span>
+          <p>
+            <FormattedMessage id="Player.club" />
+            {': '}
+            <Link className="mk-link" href={`/clubs/${player.club_id}`}>
+              {club.name}
             </Link>
           </p>
-        )}
+          {player.user_id && (
+            <p>
+              <FormattedMessage id="Player.lichess" />
+              {': '}
+              <Link
+                href={`https://lichess.org/@/${player.nickname}`}
+                target="_blank"
+                className="mk-link"
+              >
+                {player.nickname}
+              </Link>
+            </p>
+          )}
+        </div>
       </div>
     </div>
   );
