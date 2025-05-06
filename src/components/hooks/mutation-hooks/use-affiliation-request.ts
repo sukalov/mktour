70e5d81c9387e0ee4d3d1d0332051b86cebcf3c6
sukalov <matvey10@gmail.com>
2025-05-06 03:00:53 -0600

    feat: add affiliation request server action and client hook handler

---
+++ src/components/hooks/mutation-hooks/use-affiliation-request.ts
@@ -0,0 +1,15 @@
+import { requestAffiliation } from '@/lib/actions/player-affiliation';
+import { useMutation } from '@tanstack/react-query';
+import { useTranslations } from 'next-intl';
+import { toast } from 'sonner';
+
+export default function useAffiliationRequestMutation() {
+  const t = useTranslations('Toasts');
+  return useMutation({
+    mutationFn: requestAffiliation,
+    onSuccess: () => {
+      toast.success(t('affiliation requested'));
+    },
+    onError: () => toast.error(t('server error')),
+  });
+}
