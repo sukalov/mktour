82a76860a9979d6b746b3d1fb10f0dc446426a97
Mark <strategy155@gmail.com>
2025-05-11 20:15:29 +0200

    added warn instead of errror for eslint config unused vars

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,8 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
 import { defineConfig } from "eslint/config";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -16,7 +16,7 @@ export default defineConfig([{
     extends: compat.extends("next/core-web-vitals", "prettier"),
 
     rules: {
-        "no-unused-vars": ["error", {
+        "no-unused-vars": ["warn", { // #TODO: ASAP change it back to error 
             vars: "all",
             args: "all",
             ignoreRestSiblings: true,
