f6bef2acc75a21d72b1f461b53796987d3a74010
Anatoly <yatskovanatoly@gmail.com>
2025-05-17 01:15:26 +0300

    Draft: inbox items

--- src/app/clubs/my/(tabs)/inbox.tsx
+++ src/app/clubs/my/(tabs)/inbox.tsx
@@ -3,8 +3,12 @@
 import { ClubTabProps } from '@/app/clubs/my/tabMap';
 import Empty from '@/components/empty';
 import { useClubNotifications } from '@/components/hooks/query-hooks/use-club-notifications';
+import { useUser } from '@/components/hooks/query-hooks/use-user';
+import { Button } from '@/components/ui/button';
+import { Card } from '@/components/ui/card';
 import { DatabaseNotification } from '@/lib/db/schema/notifications';
 import { DatabaseAffiliation } from '@/lib/db/schema/players';
+import { Check, X } from 'lucide-react';
 import { useTranslations } from 'next-intl';
 
 const ClubInbox = ({ selectedClub }: Pick<ClubTabProps, 'selectedClub'>) => {
@@ -16,11 +20,18 @@ const ClubInbox = ({ selectedClub }: Pick<ClubTabProps, 'selectedClub'>) => {
   if (notifications.data.length < 1)
     return <Empty className="p-2">{t('empty')}</Empty>;
   return (
-    <div className="mk-list">{notifications.data.map(NotificationItem)}</div>
+    <div className="mk-list">
+      {notifications.data.map(NotificationItemIteratee)}
+    </div>
   );
 };
 
 export default ClubInbox;
+
+const NotificationItemIteratee = (data: {
+  notification: DatabaseNotification;
+  affiliation: DatabaseAffiliation | null;
+}) => <NotificationItem key={data.affiliation?.id} {...data} />;
 
 const NotificationItem = ({
   notification,
@@ -29,9 +40,34 @@ const NotificationItem = ({
   notification: DatabaseNotification;
   affiliation: DatabaseAffiliation | null;
 }) => {
+  const user = useUser(notification.user_id);
+
   return (
-    <div className="py-8" key={notification.id}>
-      {JSON.stringify({ notification, affiliation }, null, 4)}
-    </div>
+    <Card className="mk-card" key={notification.id}>
+      <div className="flex items-center justify-between">
+        <div>
+          <p className="text-sm text-gray-600">
+            {notification.notification_type === 'affiliation_request'
+              ? `Affiliation request from user ${user.data?.username}`
+              : 'Unknown notification type'}
+          </p>
+          <p className="text-xs text-gray-500">
+            Created at: {new Date(notification.created_at).toLocaleString()}
+          </p>
+        </div>
+        <Button size={'icon'}>
+          <Check />{' '}
+        </Button>
+        <Button variant={'destructive'} size={'icon'}>
+          <X />{' '}
+        </Button>
+      </div>
+      {affiliation && (
+        <div className="mt-2 text-sm text-gray-700">
+          <p>Affiliation ID: {notification.id}</p>
+          <p>Status: {affiliation.status}</p>
+        </div>
+      )}
+    </Card>
   );
 };
