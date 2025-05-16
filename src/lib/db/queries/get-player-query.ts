0b283469837ac6261a57f3d77aa8b53724b28862
sukalov <matvey10@gmail.com>
2025-05-16 15:33:20 -0600

    chore: fix remaining leftjoins for consistency. only, two leftJoint are intentional. others made innerJoins

--- src/lib/db/queries/get-player-query.ts
+++ src/lib/db/queries/get-player-query.ts
@@ -1,25 +1,25 @@
 import { db } from '@/lib/db';
-import { DatabaseClub, clubs } from '@/lib/db/schema/clubs';
+import { clubs, DatabaseClub } from '@/lib/db/schema/clubs';
 import { DatabasePlayer, players } from '@/lib/db/schema/players';
 import { DatabaseUser, users } from '@/lib/db/schema/users';
 import { eq } from 'drizzle-orm';
 
 export default async function getPlayerQuery(
   id: string,
-): Promise<PlayerToClubLeftJoin> {
+): Promise<PlayerToClubJoin | undefined> {
   const result = (
     await db
       .select()
       .from(players)
       .where(eq(players.id, id))
-      .leftJoin(clubs, eq(players.club_id, clubs.id))
+      .innerJoin(clubs, eq(players.club_id, clubs.id))
       .leftJoin(users, eq(players.user_id, users.id))
-  ).at(0) as PlayerToClubLeftJoin;
+  ).at(0);
 
   return result;
 }
 
-interface PlayerToClubLeftJoin {
+interface PlayerToClubJoin {
   player: DatabasePlayer;
   club: DatabaseClub;
   user: DatabaseUser | null;
