9b0e4efa78277cbd9532370108cffeef7ff8054e
Anatoly <yatskovanatoly@gmail.com>
2025-05-19 01:46:22 +0300

    fixme!: check for user in affiliation handler

--- src/components/notification-items.tsx
+++ src/components/notification-items.tsx
@@ -1,18 +1,47 @@
 'use client';
 
+import { LoadingSpinner } from '@/app/loading';
 import FormattedMessage from '@/components/formatted-message';
+import useAffiliationAcceptMutation from '@/components/hooks/mutation-hooks/use-affiliation-accept';
+import useAffiliationRejectMutation from '@/components/hooks/mutation-hooks/use-affiliation-reject';
 import { Button } from '@/components/ui/button';
 import { Card } from '@/components/ui/card';
 import { AffiliationNotification } from '@/lib/actions/get-user-notifications';
+import { useQueryClient } from '@tanstack/react-query';
 import { Check, Pointer, UserRound, X } from 'lucide-react';
 import Link from 'next/link';
 
 export const AffiliationNotificationLi = ({
   affiliation,
   notification,
-  user,
   player,
+  user,
 }: AffiliationNotification) => {
+  const queryClient = useQueryClient();
+  const { mutate: acceptAffiliation, isPending: pendingAccept } =
+    useAffiliationAcceptMutation({
+      queryClient,
+    });
+  const { mutate: rejectMutation, isPending: pendingReject } =
+    useAffiliationRejectMutation({
+      queryClient,
+    });
+
+  const isPending = pendingAccept || pendingReject;
+
+  if (!user || !affiliation) return null;
+
+  const variables = {
+    // userId
+    affiliationId: affiliation.id,
+    notificationId: notification.id,
+  };
+
+  const handleAccept = (accept: boolean) => {
+    const fn = accept ? acceptAffiliation : rejectMutation;
+    fn(variables);
+  };
+
   if (!affiliation) return null;
   return (
     <Card className="mk-card flex flex-col gap-2" key={notification.id}>
@@ -36,14 +65,18 @@ export const AffiliationNotificationLi = ({
             </div>
           </div>
         </div>
-        <div className="ml-3 flex gap-2">
-          <Button>
-            <Check />{' '}
-          </Button>
-          <Button variant="destructive">
-            <X />{' '}
-          </Button>
-        </div>
+        {isPending ? (
+          <LoadingSpinner />
+        ) : (
+          <div className="ml-3 flex gap-2">
+            <Button onClick={() => handleAccept(true)} variant="secondary">
+              <Check />{' '}
+            </Button>
+            <Button onClick={() => handleAccept(false)} variant="destructive">
+              <X />{' '}
+            </Button>
+          </div>
+        )}
       </div>
     </Card>
   );
