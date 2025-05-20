be9f5aee4a084f83c36e500e82affe1d9178cf47
sukalov <matvey10@gmail.com>
2025-05-20 02:41:23 -0600

    feat: add one player procedure to fix panel

--- src/server/api/routers/player.ts
+++ src/server/api/routers/player.ts
@@ -1,1 +1,13 @@
-export const playerRouter = {};
+import getPlayersLastTmts from '@/server/db/queries/get-players-last-tmts';
+import { publicProcedure } from '@/server/trpc';
+import { z } from 'zod';
+
+export const playerRouter = {
+  playersLastTournaments: publicProcedure
+    .input(z.object({ playerId: z.string() }))
+    .query(async (opts) => {
+      const { input } = opts;
+      const result = await getPlayersLastTmts(input.playerId);
+      return result;
+    }),
+};
