aba419736dcdde05f8249e3619c972186ea2e016
sukalov <matvey10@gmail.com>
2025-05-21 19:19:35 -0600

    fix: https://nextjs.org/docs/messages/ppr-caught-error

--- src/app/clubs/[id]/page.tsx
+++ src/app/clubs/[id]/page.tsx
@@ -1,9 +1,11 @@
 import ClubPage from '@/app/clubs/[id]/club';
 import { publicCaller } from '@/server/api';
+import { unstable_noStore } from 'next/cache';
 import { notFound } from 'next/navigation';
 
 export default async function Page(props: ClubPageProps) {
   const params = await props.params;
+  unstable_noStore();
   let club;
   try {
     club = await publicCaller.club.info({ clubId: params.id });
