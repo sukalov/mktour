de6313f02b74f43918e9324b0f186dcbe164244d
sukalov <matvey10@gmail.com>
2025-05-21 21:00:50 -0600

    feat: add suspense to club-page. faster response achieved

--- src/app/clubs/[id]/page.tsx
+++ src/app/clubs/[id]/page.tsx
@@ -1,11 +1,17 @@
 import ClubPage from '@/app/clubs/[id]/club';
+import Loading from '@/app/loading';
 import { publicCaller } from '@/server/api';
+import { Suspense } from 'react';
 
 export default async function Page(props: ClubPageProps) {
   const params = await props.params;
   const clubPromise = publicCaller.club.info({ clubId: params.id });
 
-  return <ClubPage clubPromise={clubPromise} />;
+  return (
+    <Suspense fallback={<Loading />}>
+      <ClubPage clubPromise={clubPromise} />
+    </Suspense>
+  );
 }
 
 export interface ClubPageProps {
