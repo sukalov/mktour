4ee56e1f71a60225fb3db1a8f0a23548c32acfc3
sukalov <matvey10@gmail.com>
2025-05-22 00:20:59 -0600

    fix: return undefined if clubInfo not found

--- src/app/clubs/[id]/page.tsx
+++ src/app/clubs/[id]/page.tsx
@@ -1,18 +1,12 @@
 import ClubPage from '@/app/clubs/[id]/club';
 import { publicCaller } from '@/server/api';
-import { unstable_noStore } from 'next/cache';
 import { notFound } from 'next/navigation';
 
 export default async function Page(props: ClubPageProps) {
   const params = await props.params;
-  unstable_noStore();
-  try {
-    const club = await publicCaller.club.info({ clubId: params.id });
-    return <ClubPage club={club} />;
-  } catch (e) {
-    console.log(e);
-    notFound();
-  }
+  const club = await publicCaller.club.info({ clubId: params.id });
+  if (!club) notFound();
+  return <ClubPage club={club} />;
 }
 
 export interface ClubPageProps {
