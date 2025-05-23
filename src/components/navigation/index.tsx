dd471fc32446352dc82a497cfc1c80cf471433c1
Anatoly <yatskovanatoly@gmail.com>
2025-05-23 03:01:34 +0300

    feat: global mk spacing variable

--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -10,7 +10,7 @@ import { FC, useContext } from 'react';
 const Navigation: FC<NavMenuProps> = ({ user }) => {
   const { isTablet } = useContext(MediaQueryContext);
   return (
-    <nav className="bg-background fixed z-50 flex h-14 w-full items-center justify-between border-b p-3">
+    <nav className="bg-background p-mk fixed z-50 flex h-14 w-full items-center justify-between border-b">
       <MktourNavbar />
       {isTablet ? <Mobile user={user} /> : <Desktop user={user} />}
     </nav>
