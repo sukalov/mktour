09956129d2125f4bb72ad43d2a3562c6a8cb8179
sukalov <matvey10@gmail.com>
2025-05-27 13:06:33 -0600

    feat: cache homepage

--- src/app/(routes)/authorized.tsx
+++ src/app/(routes)/authorized.tsx
@@ -1,17 +1,21 @@
-'use server';
-
 import MakeTournamentButton from '@/components/button-make-tournament';
 import TeamJoinToaster from '@/components/team-join-toaster';
 import '@/styles/cursor.css';
 import { cookies } from 'next/headers';
 
 export default async function Authorized() {
-  const isNew = (await cookies()).get('show_new_user_toast');
+  'use cache';
 
   return (
     <div className="flex min-h-[calc(100svh-3.5rem)] w-full flex-auto items-center justify-center p-4">
       <MakeTournamentButton />
-      {isNew && <TeamJoinToaster />}
+      <TeamJoinToasterServer />
     </div>
   );
 }
+
+const TeamJoinToasterServer = async () => {
+  const isNew = (await cookies()).get('show_new_user_toast');
+  if (!isNew) return null;
+  return <TeamJoinToaster />;
+};
