5cb0a1f228e7c24ee81503b794f49329a050a30d
sukalov <matvey10@gmail.com>
2025-06-20 22:23:49 +0300

    fix: broken redirect FROM when signing in due to updated searchParams types

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,15 +1,19 @@
 import SignInWithLichessButton from '@/components/auth/sign-in-with-lichess-button';
 import { publicCaller } from '@/server/api';
+import { SearchParams } from 'next/dist/server/request/search-params';
 import { redirect } from 'next/navigation';
 import { Suspense } from 'react';
 
-const PageContent = async (props: { searchParams: Promise<string> }) => {
+const PageContent = async (props: { searchParams: Promise<SearchParams> }) => {
   const searchParams = await props.searchParams;
+
+  const from =
+    typeof searchParams.from === 'string' ? searchParams.from : undefined;
   const user = await publicCaller.user.auth();
   if (user) redirect('/');
   return (
     <main className="mx-auto my-4 flex h-[calc(100svh-3.5rem)] w-full max-w-lg flex-auto items-center justify-center p-10">
-      <SignInWithLichessButton className="p-10 py-16" from={searchParams} />
+      <SignInWithLichessButton className="p-10 py-16" from={from} />
     </main>
   );
 };
