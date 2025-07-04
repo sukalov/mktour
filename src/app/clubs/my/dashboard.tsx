ec1f79a0179a076380cbf5173603a4efd964da8e
Anatoly <yatskovanatoly@gmail.com>
2025-07-04 20:09:55 +0300

    feat: fab for club dashboard

--- src/app/clubs/my/dashboard.tsx
+++ src/app/clubs/my/dashboard.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import AddPlayerDrawer from '@/app/clubs/my/add-new-player';
 import ClubSelect from '@/app/clubs/my/club-select';
 import ClubDashboardTabList from '@/app/clubs/my/dashboard-tab-list';
 import { ClubDashboardTab, ClubTabProps, tabMap } from '@/app/clubs/my/tabMap';
@@ -7,8 +8,9 @@ import Loading from '@/app/loading';
 import Empty from '@/components/empty';
 import { useUser } from '@/components/hooks/query-hooks/use-user';
 import SwipeHandlerProvider from '@/components/swipe-handler-provider';
+import FabProvider from '@/components/ui/fab-provider';
 import { useTranslations } from 'next-intl';
-import { Dispatch, FC, SetStateAction, useState } from 'react';
+import { Dispatch, FC, ReactNode, SetStateAction, useState } from 'react';
 
 export default function Dashboard({ userId }: { userId: string }) {
   const t = useTranslations('Club.Dashboard');
@@ -17,6 +19,7 @@ export default function Dashboard({ userId }: { userId: string }) {
   const ActiveTab: FC<ClubTabProps> = tabMap[tab];
   const tabs = Object.keys(tabMap) as ClubDashboardTab[];
   const indexOfTab = tabs.indexOf(tab);
+  const fabContent = fabTabMap[tab];
 
   if (!data && isLoading) return <Loading />;
   if (!data) return <Empty>{t('no data')}</Empty>;
@@ -25,6 +28,7 @@ export default function Dashboard({ userId }: { userId: string }) {
     <SwipeHandlerProvider
       handleSwipe={(dir) => handleSwipe(dir, indexOfTab, tabs, setTab)}
     >
+      <FabProvider status="organizer" fabContent={fabContent} />
       <div className="fixed top-14 z-10 w-full">
         <ClubDashboardTabList
           selectedClub={data.selected_club}
@@ -34,7 +38,7 @@ export default function Dashboard({ userId }: { userId: string }) {
         <ClubSelect user={data} />
       </div>
       <div className="mk-container relative pt-20">
-        <ActiveTab selectedClub={data.selected_club} userId={userId} />
+        <ActiveTab userId={userId} selectedClub={data.selected_club} />
       </div>
     </SwipeHandlerProvider>
   );
@@ -54,4 +58,12 @@ const handleSwipe = (
   } else return;
 
   setTab(tabs[newIndex]);
+};
+
+const fabTabMap: Record<ClubDashboardTab, ReactNode | undefined> = {
+  players: <AddPlayerDrawer />,
+  tournaments: undefined,
+  main: undefined,
+  inbox: undefined,
+  settings: undefined,
 };
