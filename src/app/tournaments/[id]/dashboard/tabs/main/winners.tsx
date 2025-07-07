fdf03af6cf7204a3c55f95f2d346ace3cfcf5c42
Anatoly <yatskovanatoly@gmail.com>
2025-07-07 18:20:12 +0300

    feat: group winners under their medals

--- src/app/tournaments/[id]/dashboard/tabs/main/winners.tsx
+++ src/app/tournaments/[id]/dashboard/tabs/main/winners.tsx
@@ -7,32 +7,71 @@ import { FC } from 'react';
 
 const Winners: FC<TournamentInfo> = ({ tournament }) => {
   const { data: players } = useTournamentPlayers(tournament.id);
-  const winners = players?.filter(({ place }) => place && place <= 3);
+  const winners = groupWinnersByPlace(players);
 
   if (!winners || !tournament.closed_at) return null;
   return (
-    <div className="mk-link flex flex-col gap-8">
-      {winners.map((player) => (
-        <Player key={player.id} {...player} />
+    <div className="flex flex-col gap-8">
+      {Object.entries(winners).map(([place, players]) => (
+        <MedalGroup key={place} place={place} players={players} />
       ))}
     </div>
   );
 };
 
-const Player: FC<PlayerModel> = ({ id, nickname, place }) => {
-  if (!place) return null;
+const MedalGroup: FC<{ place: string; players: PlayerModel[] }> = ({
+  place,
+  players,
+}) => {
   return (
-    <div className="flex items-center gap-2 truncate">
-      <Medal className={`size-6 ${medalColour[place - 1]}`} />
-      <Link href={`/player/${id}`}> {nickname}</Link>
+    <div className="flex items-start gap-2 truncate">
+      <Medal className={`size-6 ${medalColour[parseInt(place) - 1]}`} />
+      <div className="flex flex-col gap-2">
+        {players.map(({ id, nickname }, i) => {
+          const shouldShowSeparator =
+            players.length > 1 && i < players.length - 1;
+
+          return (
+            <div key={id}>
+              <Link href={`/player/${id}`} className="mk-link">
+                {' '}
+                {nickname}
+              </Link>
+              {shouldShowSeparator && ','}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
+
+// const PlayersIteratee = ({ id, place, nickname }: PlayerModel) => {
+//   if (!place) return null;
+//   return <Link href={`/player/${id}`}> {nickname}</Link>;
+// };
 
 export const Medal: FC<{ className: string }> = ({ className }) => (
   <div className={`aspect-square rounded-full ${className}`} />
 );
 
 export const medalColour = ['bg-amber-300', 'bg-gray-300', 'bg-amber-700'];
+
+const groupWinnersByPlace = (players: PlayerModel[] | undefined) => {
+  const winners = players?.filter(({ place }) => place && place <= 3);
+
+  if (!winners) return {};
+  return winners.reduce(
+    (acc, player) => {
+      const place = player.place || 0;
+      if (!acc[place]) {
+        acc[place] = [];
+      }
+      acc[place].push(player);
+      return acc;
+    },
+    {} as Record<number, PlayerModel[]>,
+  );
+};
 
 export default Winners;
