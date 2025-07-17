997735de7c45ab01affa7bbb859d7b6205be9d76
Anatoly <yatskovanatoly@gmail.com>
2025-07-17 18:51:52 +0300

    fix: get-user-club-affiliation won't return undefined

--- src/server/queries/get-user-club-affiliation.ts
+++ src/server/queries/get-user-club-affiliation.ts
@@ -3,17 +3,20 @@ import { affiliations, DatabaseAffiliation } from '@/server/db/schema/players';
 import { and, eq } from 'drizzle-orm';
 import { User } from 'lucia';
 
-export const getUserClubAffiliation = async (
+export async function getUserClubAffiliation(
   user: User | null,
   clubId: string,
-): Promise<DatabaseAffiliation | undefined> => {
-  if (!user) throw new Error('USER_NOT_FOUND');
-  const res = await db
+): Promise<DatabaseAffiliation | undefined> {
+  if (!user) {
+    throw new Error('USER_NOT_FOUND');
+  }
+
+  const [affiliation] = await db
     .select()
     .from(affiliations)
     .where(
       and(eq(affiliations.club_id, clubId), eq(affiliations.user_id, user.id)),
     );
 
-  return res[0] ?? undefined;
-};
+  return affiliation;
+}
