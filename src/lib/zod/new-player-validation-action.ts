503eca23e40408ff968c7c9d011dddec1098b661
sukalov <matvey10@gmail.com>
2025-10-09 17:35:17 +0300

    fix: global scope for unique player nickname

--- src/lib/zod/new-player-validation-action.ts
+++ src/lib/zod/new-player-validation-action.ts
@@ -2,13 +2,24 @@
 
 import { db } from '@/server/db';
 import { players } from '@/server/db/schema/players';
-import { sql } from 'drizzle-orm';
+import { and, eq, sql } from 'drizzle-orm';
 
-export async function validateNewPlayer({ name }: { name: string }) {
+export async function validateNewPlayer({
+  name,
+  club_id,
+}: {
+  name: string;
+  club_id: string;
+}) {
   const isValid = await db
     .select()
     .from(players)
-    .where(sql`lower(${players.nickname}) = ${name.toLowerCase()}`)
+    .where(
+      and(
+        sql`lower(${players.nickname}) = ${name.toLowerCase()}`,
+        eq(players.club_id, club_id),
+      ),
+    )
     .get();
 
   return isValid === undefined;
