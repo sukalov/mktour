7cc9a6aea0a8d8d63a621b2f2a4876540bd28f1e
sukalov <matvey10@gmail.com>
2025-11-05 21:27:47 -0700

    fix: lichess logo size inside signin button

--- src/components/auth/sign-in-with-lichess-button.tsx
+++ src/components/auth/sign-in-with-lichess-button.tsx
@@ -17,7 +17,7 @@ export default async function SignInWithLichessButton({
   return (
     <Button
       className={cn(
-        `m-auto flex h-28 min-h-28 w-full max-w-md flex-none flex-col gap-2 px-1 font-bold [&_svg]:size-10`,
+        `m-auto flex h-28 min-h-28 w-full max-w-md flex-none flex-col gap-2 px-1 font-bold`,
         className,
       )}
       variant="outline"
@@ -29,7 +29,7 @@ export default async function SignInWithLichessButton({
       >
         <div className="grid-flow-col"></div>
         <span className="grid-col-3">
-          <LichessLogo />
+          <LichessLogo className="size-10" />
         </span>
         <span className="grid-col--9 text-[1.4rem] leading-none font-light md:px-4 md:text-xl">
           {t('Home.sign in with lichess')}
