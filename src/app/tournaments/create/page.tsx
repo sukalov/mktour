c935d91828f4052ade42b3ba510655f710af651e
sukalov <matvey10@gmail.com>
2025-11-10 15:15:33 -0700

    exp: rename loading to loading.tsx, remove explicit suspense

--- src/app/tournaments/create/page.tsx
+++ src/app/tournaments/create/page.tsx
@@ -1,19 +1,11 @@
-import Loading from '@/app/tournaments/create/loading-form';
 import NewTournamentForm from '@/app/tournaments/create/new-tournament-form';
 import { publicCaller } from '@/server/api';
 import { redirect } from 'next/navigation';
-import { Suspense } from 'react';
 
 export default async function NewTournament() {
   const user = await publicCaller.user.auth();
   if (!user) redirect('/sign-in?from=/tournaments/create');
   const userClubs = await publicCaller.user.clubs({ userId: user.id });
 
-  return (
-    <Suspense fallback={<Loading />}>
-      <div className="w-full">
-        <NewTournamentForm clubs={userClubs} user={user} />
-      </div>
-    </Suspense>
-  );
+  return <NewTournamentForm clubs={userClubs} user={user} />;
 }
