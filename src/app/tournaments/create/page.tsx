2fe60cc5eac764b5581bf8f42a1d73f512bbed2f
sukalov <matvey10@gmail.com>
2025-11-10 15:22:26 -0700

    exp: add suspense

--- src/app/tournaments/create/page.tsx
+++ src/app/tournaments/create/page.tsx
@@ -1,11 +1,16 @@
 import NewTournamentForm from '@/app/tournaments/create/new-tournament-form';
 import { publicCaller } from '@/server/api';
 import { redirect } from 'next/navigation';
+import { Suspense } from 'react';
 
 export default async function NewTournament() {
   const user = await publicCaller.user.auth();
   if (!user) redirect('/sign-in?from=/tournaments/create');
   const userClubs = await publicCaller.user.clubs({ userId: user.id });
 
-  return <NewTournamentForm clubs={userClubs} user={user} />;
+  return (
+    <Suspense fallback={'loading...'}>
+      <NewTournamentForm clubs={userClubs} user={user} />
+    </Suspense>
+  );
 }
