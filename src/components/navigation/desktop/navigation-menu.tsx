53f0d9621630d6e5142e472ddcc58f445e1ce736
sukalov <matvey10@gmail.com>
2025-11-14 17:43:02 -0700

    fix?: remove NavigationMenuLink element

--- src/components/navigation/desktop/navigation-menu.tsx
+++ src/components/navigation/desktop/navigation-menu.tsx
@@ -36,19 +36,17 @@ export default function NavigationMenuContainer() {
               <NavigationMenuContent>
                 <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
                   <li className="row-span-3 cursor-pointer">
-                    <NavigationMenuLink asChild>
-                      <Link
-                        className="from-muted/50 to-muted flex w-full flex-col gap-2 rounded-md bg-linear-to-b p-4 px-6 no-underline outline-hidden select-none focus:shadow-md"
-                        href={tab.path}
-                      >
-                        <div className="text-lg font-medium">
-                          {t(`Subheadings.${tab.title}`)}
-                        </div>
-                        <p className="text-muted-foreground text-sm leading-tight">
-                          {t(`Descriptions.${tab.title}`)}
-                        </p>
-                      </Link>
-                    </NavigationMenuLink>
+                    <Link
+                      className="from-muted/50 to-muted flex w-full flex-col gap-2 rounded-md bg-linear-to-b p-4 px-6 no-underline outline-hidden select-none focus:shadow-md"
+                      href={tab.path}
+                    >
+                      <div className="text-lg font-medium">
+                        {t(`Subheadings.${tab.title}`)}
+                      </div>
+                      <p className="text-muted-foreground text-sm leading-tight">
+                        {t(`Descriptions.${tab.title}`)}
+                      </p>
+                    </Link>
                   </li>
                   {tab.subMenuItems &&
                     tab.subMenuItems.map((subTab: SubNavItem) => (
