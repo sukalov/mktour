924d885c30c8a9a373c9c8fad97db661bc319cba
sukalov <matvey10@gmail.com>
2025-11-23 13:49:35 -0700

    fix?: remove explicit suspense to use only  next reserved filename

--- src/app/tournaments/create/page.tsx
+++ src/app/tournaments/create/page.tsx
@@ -1,17 +1,11 @@
-import Loading from '@/app/tournaments/create/loading';
 import NewTournamentForm from '@/app/tournaments/create/new-tournament-form';
 import { publicCaller } from '@/server/api';
 import { redirect } from 'next/navigation';
-import { Suspense } from 'react';
 
 export default async function NewTournament() {
   const user = await publicCaller.auth.info();
   if (!user) redirect('/sign-in?from=/tournaments/create');
   const userClubs = await publicCaller.user.clubs({ userId: user.id });
 
-  return (
-    <Suspense fallback={<Loading />}>
-      <NewTournamentForm clubs={userClubs} user={user} />
-    </Suspense>
-  );
+  return <NewTournamentForm clubs={userClubs} user={user} />;
 }
